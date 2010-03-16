4734c539965251ae50146c243dbdeb8bc6f9d413
Stefan Praszalowicz <deubeulyou@gmail.com>
2010-03-16 12:28:01 +0100

    initial commit indeed

---
+++ migration/go.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source ./env
+
+if ! test -z "$ENV_HAS_NOT_BEEN_EDITED"; then
+ echo "Please review and correct environment settings first !"
+ exit 1
+fi
+
+for i in part-*.sh; do
+
+ echo "Running [$i]"
+ /bin/sh $i
+ if test $? -ne 0; then
+   echo "Script [$i] failed, stopping..."
+   exit 1
+ fi
+ 
+done
---
+++ migration/part-01-dump.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+source ./env
+
+test -x "$DBXML_BINARY" || (echo "Incorrect DBXML_BINARY [$DBXML_BINARY]" && exit 1)
+test -d "$DBXML_DATA_PATH"  || (echo "Incorrect DBXML_DATA_PATH [$DBXML_DATA_PATH]" && exit 1)
+mkdir -p "$XML_DUMPS_PATH"  || (echo "Cannot create XML_DUMPS_PATH [$XML_DUMPS_PATH]" && exit 1)
+
+
+t=$(tempfile) || exit
+trap "rm -f -- '$t'" EXIT
+
+cat >$t <<_EOF_
+openContainer meps.dbxml
+cquery '*'
+print $XML_DUMPS_PATH/dump-meps.xml
+
+openContainer mps.dbxml
+cquery '*'
+print $XML_DUMPS_PATH/dump-mps.xml
+
+openContainer votes.dbxml
+cquery '*'
+print $XML_DUMPS_PATH/dump-votes.xml
+
+quit
+_EOF_
+
+
+echo "Running dbxml dump script..."
+$DBXML_BINARY -h $DBXML_DATA_PATH -s $t || (echo "Failed running dbxml script" && exit 1)
+
+exit 0
