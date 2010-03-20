fbc824ae2774a2b4d674d13fece573dbf4e6535e
Stefan Praszalowicz <deubeulyou@gmail.com>
2010-03-20 19:11:25 +0100

    added development cache of xml dumps

--- migration/part-01-dump.sh
+++ migration/part-01-dump.sh
@@ -5,6 +5,17 @@ set -e
 
 source ./env
 
+if ! test -x "$DBXML_BINARY"; then
+
+echo "No dbxml binary, using cached development dumps..."
+
+zcat cache/meps.xml.gz > $XML_DUMPS_PATH/meps.xml
+zcat cache/mps.xml.gz > $XML_DUMPS_PATH/mps.xml
+zcat cache/votes.xml.gz > $XML_DUMPS_PATH/votes.xml
+
+
+else
+
 test -x "$DBXML_BINARY" || (echo "Incorrect DBXML_BINARY [$DBXML_BINARY]" && exit 1)
 test -d "$DBXML_DATA_PATH"  || (echo "Incorrect DBXML_DATA_PATH [$DBXML_DATA_PATH]" && exit 1)
 mkdir -p "$XML_DUMPS_PATH"  || (echo "Cannot create XML_DUMPS_PATH [$XML_DUMPS_PATH]" && exit 1)
@@ -34,3 +45,4 @@ echo "Running dbxml dump script..."
 $DBXML_BINARY -h $DBXML_DATA_PATH -s $t || (echo "Failed running dbxml script" && exit 1)
 
 exit 0
+fi
