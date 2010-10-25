762be1b78b576bb6b751cb5a4c4539f9f3707db1
Stefan Praszalowicz <deubeulyou@gmail.com>
2010-10-25 09:33:49 +0200

    added django-extensions, django-debug-toolbar, south

--- memopol2/settings.py
+++ memopol2/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'memopol2.crashlog.CrashLogMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'memopol2.urls'
@@ -85,6 +86,23 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'memopol2.main',
     'memopol2.crashlog',
+	'south',
+	'django_extensions',
+	'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 #CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
