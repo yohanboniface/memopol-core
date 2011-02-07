2077de0ca5ab493da05f9306e712077c21589814
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-07 14:50:44 +0100

    use the correct test settings module name

--- scripts/django-jenkins.sh
+++ scripts/django-jenkins.sh
@@ -4,4 +4,4 @@ source ./ve/bin/activate
 pip install -q -E ./ve -r requirements.txt
 pip install -q -E ./ve -r requirements-test.txt
 python setup.py --quiet develop
-django-admin.py test --settings=memopol2.settings.test --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
+django-admin.py test --settings=memopol2.testsettings --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
