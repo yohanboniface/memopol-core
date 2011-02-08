98677ce4873408544b073b7649976f1cb5a60741
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-08 14:37:26 +0100

    also a missing part of a previous rename

--- scripts/django-jenkins.sh
+++
@@ -1,7 +0,0 @@
-cd $WORKSPACE
-virtualenv -q ve
-source ./ve/bin/activate
-pip install -q -E ./ve -r requirements.txt
-pip install -q -E ./ve -r requirements-test.txt
-python setup.py --quiet develop
-django-admin.py test --settings=memopol2.testsettings --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
