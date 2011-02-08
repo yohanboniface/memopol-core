ee6767b30e5aae4653fab094188d9ffda4b2dd3a
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-08 14:35:34 +0100

    comment jenkins script

---
+++ bin/jenkins-setup-and-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script will setup our virtualenv in the jenkins workspace, and run the tests
+
+cd $WORKSPACE
+
+# setup ve
+virtualenv -q ve
+source ./ve/bin/activate
+
+# dev install - installs requirements as well
+python setup.py --quiet develop
+
+# add our test requirements
+pip install -q -E ./ve -r requirements-test.txt
+
+# run our tests
+django-admin.py test --settings=memopol2.testsettings --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
