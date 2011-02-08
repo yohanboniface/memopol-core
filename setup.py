27277ad6ac4d0175dac388fbf9c577c7bc69a7fc
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-08 14:37:53 +0100

    good citizen setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-#!/usr/bin/env python
+import distribute_setup
+distribute_setup.use_setuptools()
 
-from distribute_setup import use_setuptools
-use_setuptools()
+import os
 from setuptools import setup, find_packages
 
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+    
 setup(
-name = "memopol",
-version = "0.1",
-packages = find_packages('.'),
-package_dir = {'':'.'},
-
-include_package_data = True,
-zip_safe = False,
-
-# author metadata
-author = 'The memopol project',
-author_email = 'deubeulyou@gmail.com',
-description = 'Memoire Politique',
-url = 'http://projets.lqdn.fr/projects/mempol',
+    name='Memopol',
+    version = '1.99.1',
+    author = 'The memopol project',
+    author_email = 'deubeulyou@gmail.com',
+    description = 'Memoire Politique',
+    long_description = read_file('README.txt'),
+    license = 'LICENSE.txt',
+    url = 'http://projets.lqdn.fr/projects/mempol',
+    packages = find_packages(),
+    include_package_data = True,
+    scripts = ['bin/jenkins-setup-and-test.sh'],
+    install_requires = read_file('requirements.txt'),
+    extras_require = {
+        'test': read_file('requirements-test.txt')
+    }
 )
