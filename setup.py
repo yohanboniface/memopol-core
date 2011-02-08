475566de8a63cc19030044507c214d421529afae
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-08 19:58:56 +0100

    find scripts automatically

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ distribute_setup.use_setuptools()
 import os
 from setuptools import setup, find_packages
 
-def read_file(filename):
-    """Read a file into a string"""
-    path = os.path.abspath(os.path.dirname(__file__))
-    filepath = os.path.join(path, filename)
-    try:
-        return open(filepath).read()
-    except IOError:
-        return ''
-
+def find_files(dirname):
+    files = []
+    for entryname in os.listdir(dirname):
+        pathname = "%s/%s" % (dirname, entryname)
+        if os.path.isfile(pathname):
+            files.append(pathname)
+        elif os.path.isdir(pathname):
+            files += find_files(pathname)        
+    return files
     
 setup(
     name='Memopol',
@@ -20,14 +20,14 @@ setup(
     author = 'The memopol project',
     author_email = 'deubeulyou@gmail.com',
     description = 'Memoire Politique',
-    long_description = read_file('README.txt'),
+    long_description = open('README.txt').read(),
     license = 'LICENSE.txt',
     url = 'http://projets.lqdn.fr/projects/mempol',
     packages = find_packages(),
     include_package_data = True,
-    scripts = ['bin/jenkins-setup-and-test.sh'],
-    install_requires = read_file('requirements.txt'),
+    scripts = find_files('bin'),
+    install_requires = open('requirements.txt').read(),
     extras_require = {
-        'test': read_file('requirements-test.txt')
+        'test': open('requirements-test.txt').read()
     }
 )
