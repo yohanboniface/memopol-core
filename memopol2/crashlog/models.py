490d16504e7478097430a242d258a03eaa958aab
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-09 14:04:58 +0100

    get rid of gettext related pylint error

--- memopol2/crashlog/models.py
+++ memopol2/crashlog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 import datetime
 
 class ErrorBatch(models.Model):
     objects = models.Manager()
-    class_name      = models.CharField(_('Type'), max_length=128)
+    class_name      = models.CharField('Type', max_length=128)
     message         = models.TextField()
     traceback       = models.TextField()
     times_seen      = models.PositiveIntegerField(default=1)
@@ -20,10 +19,9 @@ class ErrorBatch(models.Model):
 
 class Error(models.Model):
     objects = models.Manager()
-    class_name      = models.CharField(_('type'), max_length=128)
+    class_name      = models.CharField('type', max_length=128)
     message         = models.TextField()
     traceback       = models.TextField()
     datetime        = models.DateTimeField(default=datetime.datetime.now)
     url             = models.URLField(verify_exists=False, null=True, blank=True)
     server_name     = models.CharField(max_length=128, db_index=True)
-    
