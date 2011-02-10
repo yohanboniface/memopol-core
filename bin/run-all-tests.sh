5154a989c9b8c8feb1f66cb521954308b31cee21
Stefan Praszalowicz <deubeulyou@gmail.com>
2011-02-10 02:09:11 +0100

    set lettuce verbosity to 3

--- bin/run-all-tests.sh
+++ bin/run-all-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 django-admin.py test --settings=memopol2.testsettings --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
-django-admin.py harvest  --settings=memopol2.testsettings  -a memopol2.main
+django-admin.py harvest  --settings=memopol2.testsettings --verbosity=3 -a memopol2.main
 pylint --rcfile=.pylintrc memopol2 | tee pylint.out
