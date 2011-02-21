e80a9a2ee47910db909b2c1cf5d995414120c537
David Larlet <larlet@gmail.com>
2011-02-21 23:16:24 +0100

    Always use named urls, useful in both templates and models (for permalinks)

--- memopol2/urls.py
+++ memopol2/urls.py
@@ -1,41 +1,38 @@
 import os
 
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('', # pylint: disable=C0103
-    # Example:
-    # (r'^memopol2/', include('memopol2.foo.urls')),
+    url(r'^$', 'memopol2.main.views.index_countries', name='index'),
 
-    (r'^$', 'memopol2.main.views.index_countries'),
-
-    (r'^names/$', 'memopol2.main.views.index_names'),
-    (r'^countries/$', 'memopol2.main.views.index_countries'),
-    (r'^country/(?P<country_code>[a-zA-Z][a-zA-Z])/$', 'memopol2.main.views.index_by_country'),
-    (r'^groups/$', 'memopol2.main.views.index_groups'),
-    (r'^group/(?P<group>[a-zA-Z/-]+)/$', 'memopol2.main.views.index_by_group'),
+    url(r'^names/$', 'memopol2.main.views.index_names', name='index_names'),
+    url(r'^countries/$', 'memopol2.main.views.index_countries', name='index_countries'),
+    url(r'^country/(?P<country_code>[a-zA-Z][a-zA-Z])/$', 'memopol2.main.views.index_by_country', name='index_by_country'),
+    url(r'^groups/$', 'memopol2.main.views.index_groups', name='index_groups'),
+    url(r'^group/(?P<group>[a-zA-Z/-]+)/$', 'memopol2.main.views.index_by_group', name='index_by_group'),
 
 
-    (r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/$', 'memopol2.main.views.mep'),
-    (r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/raw/$', 'memopol2.main.views.mep_raw'),
-    (r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/addposition/$', 'memopol2.main.views.mep_addposition'),
+    url(r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/$', 'memopol2.main.views.mep', name='mep'),
+    url(r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/raw/$', 'memopol2.main.views.mep_raw', name='mep_raw'),
+    url(r'^mep/(?P<mep_id>[a-zA-Z0-9]+)/addposition/$', 'memopol2.main.views.mep_addposition', name='mep_addposition'),
 
 
-    (r'^moderation/$', 'memopol2.main.views.moderation'),
-    (r'^moderation/get_unmoderated_positions$', 'memopol2.main.views.moderation_get_unmoderated_positions'),
-    (r'^moderation/moderate_position$', 'memopol2.main.views.moderation_moderate_positions'),
+    url(r'^moderation/$', 'memopol2.main.views.moderation', name='moderation'),
+    url(r'^moderation/get_unmoderated_positions$', 'memopol2.main.views.moderation_get_unmoderated_positions', name='moderation_get_unmoderated_positions'),
+    url(r'^moderation/moderate_position$', 'memopol2.main.views.moderation_moderate_positions', name='moderation_moderate_positions'),
 
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 
 )
 
 # hack to autodiscover static files location in dev mode
 if settings.DEBUG:
     urlpatterns += patterns('',
-    (r'^static/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static')}),
+        url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static')}),
     )
 # TODO: static files location in production
 # should never be served by django, settings.MEDIA_URL is the right way to do
