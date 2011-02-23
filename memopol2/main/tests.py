189f55e50c14fce9d01a4ed123b19d0582ea9879
David Larlet <larlet@gmail.com>
2011-02-23 14:54:11 +0100

    Adding some tests before switching to permanent couchdb views, always better to know what you broke

--- memopol2/main/tests.py
+++ memopol2/main/tests.py
@@ -1,23 +1,63 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
-Replace these with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ViewsTest(TestCase):
+    """
+    Check basic context of indexes' views.
+    """
+    def test_index_names(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests index_names context.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        client = Client()
+        response = client.get(reverse("index_names"))
+        self.failUnlessEqual(len(response.context['meps_list']), 1194)
+        self.failUnlessEqual(repr(response.context['meps_list'][0]), "{u'value': {u'group': u'ECR', u'last': u'Bielan', u'first': u'Adam'}, u'id': u'AdamBielan', u'key': None}")
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_index_groups(self):
+        """
+        Tests index_groups context.
+        """
+        client = Client()
+        response = client.get(reverse("index_groups"))
+        self.failUnlessEqual(len(response.context['groups']), 12)
+        self.failUnlessEqual(repr(response.context['groups'][0]), '{u\'value\': {u\'count\': 144, u\'name\': u"Groupe Alliance des d\\xe9mocrates et des lib\\xe9raux pour l\'Europe"}, u\'key\': u\'ALDE\'}')
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_index_countries(self):
+        """
+        Tests index_countries context.
+        """
+        client = Client()
+        response = client.get(reverse("index_countries"))
+        self.failUnlessEqual(len(response.context['countries']), 27)
+        self.failUnlessEqual(repr(response.context['countries'][0]), "{u'value': {u'count': 141, u'code': u'DE'}, u'key': u'Allemagne'}")
+
+    def test_index_by_country(self):
+        """
+        Tests index_by_country context.
+        """
+        client = Client()
+        response = client.get(reverse("index_by_country", args=('DE',)))
+        self.failUnlessEqual(len(response.context['meps_list']), 141)
+        self.failUnlessEqual(repr(response.context['meps_list'][0]), "{u'value': {u'group': u'PPE', u'last': u'Dess', u'first': u'Albert'}, u'id': u'AlbertDess', u'key': u'DE'}")
+
+    def test_index_by_group(self):
+        """
+        Tests index_by_group context.
+        """
+        client = Client()
+        response = client.get(reverse("index_by_group", args=('ALDE',)))
+        self.failUnlessEqual(len(response.context['meps_list']), 144)
+        self.failUnlessEqual(repr(response.context['meps_list'][0]), "{u'value': {u'group': u'ALDE', u'last': u'V\\u0103lean', u'first': u'Adina-Ioana'}, u'id': u'AdinaIoanaValean', u'key': u'ALDE'}")
+
+    def test_mep(self):
+        """
+        Tests mep context.
+        """
+        client = Client()
+        response = client.get(reverse("mep", args=('AlbertDess',)))
+        self.failUnlessEqual(repr(response.context['d'].keys()), "[u'activities', u'functions', u'_rev', u'extid', u'contact', u'scores', u'infos', u'_id', u'cv']")
+        self.failUnlessEqual(repr(response.context['d']['cv']['position'][-1]), 'u"M\\xe9daill\\xe9 de l\'ordre bavarois du M\\xe9rite (2007)."')
+        self.failUnlessEqual(str(response.context['d']['contact']['address'][0]['street']), '60, rue Wiertz')
+        self.failUnlessEqual(repr(response.context['positions']), "[]")
 
--- memopol2/urls.py
+++ memopol2/urls.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 
 from django.conf.urls.defaults import patterns, include, url
