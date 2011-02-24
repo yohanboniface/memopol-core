0edbd5b0eda5312cb623255802d9cd862c820047
David Larlet <larlet@gmail.com>
2011-02-24 12:26:26 +0100

    Consistency in views/templates names of the votes app

--- apps/votes/urls.py
+++ apps/votes/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 from votes import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index_votes, name='index'),
-    url(r'^(?P<vote_name>[a-zA-Z/-_]+)/$', views.vote, name='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<vote_name>[a-zA-Z/-_]+)/$', views.detail, name='detail'),
 )
--- apps/votes/views.py
+++ apps/votes/views.py
@@ -2,17 +2,17 @@ from django.views.generic.simple import direct_to_template
 
 from votes.models import Vote
 
-def index_votes(request):
+def index(request):
     votes = Vote.view('main/all')
     context = {
         'votes': votes,
     }
-    return direct_to_template(request, 'votes/votes.html', context)
+    return direct_to_template(request, 'votes/index.html', context)
 
-def vote(request, vote_name):
+def detail(request, vote_name):
     votes = Vote.view('main/by_name', key=vote_name)
     context = {
         'vote': votes.first(),
     }
-    return direct_to_template(request, 'votes/vote.html', context)
+    return direct_to_template(request, 'votes/detail.html', context)
 
