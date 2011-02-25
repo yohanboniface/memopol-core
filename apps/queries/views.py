830bf6fb37b389bbf0f40bb17dd7ed061f2fb1a9
stef <stefan.marsiske@gmail.com>
2011-02-25 21:42:05 +0100

    merged psycojokers query changes

--- apps/queries/views.py
+++ apps/queries/views.py
@@ -11,8 +11,7 @@ def query(request):
     form = QueryForm(request.GET)
     if not form.is_valid():
         return render_to_response('query.html')
-    print form.cleaned_data
-    #meps = MEP.view('meps/query', { 'key': form.cleaned_data.get('commitee_filter')})
+    meps = MEP.view('meps/query', key=form.cleaned_data.get('commitee_filter'))
     #return render_to_response('query.html', { 'meps': meps})
     return render_to_response('query.html')
 
