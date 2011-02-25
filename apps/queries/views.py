52dc6dcb606d365f8e12b24c7777736bdc856b3f
stef <stefan.marsiske@gmail.com>
2011-02-25 22:34:36 +0100

    [mod] query returning stg, display still wrong

--- apps/queries/views.py
+++ apps/queries/views.py
@@ -12,8 +12,7 @@ def query(request):
     if not form.is_valid():
         return render_to_response('query.html')
     meps = MEP.view('meps/query', key=form.cleaned_data.get('commitee_filter'))
-    #return render_to_response('query.html', { 'meps': meps})
-    return render_to_response('query.html')
+    return render_to_response('query.html', { 'meps': meps})
 
 def bla(request):
     meps_by_name = MEP.view('meps/by_name')
