6828ee827625408964d412cca32e3944cef01261
Laurent Peuch <cortex@worlddomination.be>
2011-02-27 00:19:55 +0100

    [enh] add xlabel and ylabel to mep graph to make it clearer

--- apps/meps/views.py
+++ apps/meps/views.py
@@ -91,6 +91,8 @@ def mep(request, mep_id):
             pyplot.axis([0, len(scores) - 1, 0, 102])
             pyplot.title("%s - Votes notes evolution over time" % (mep_.infos['name']['full']))
             pyplot.xticks(range(len(scores)), [k['date'] for k in score_list])
+            pyplot.xlabel("Votes dates")
+            pyplot.ylabel("Scores on votes")
             pyplot.savefig(realpath(".%simg/trends/meps/%s-scores.png" % (settings.MEDIA_URL, mep_id)), format="png")
             pyplot.clf()
         except ImportError:
