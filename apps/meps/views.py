5defecc30f5ae4797051fc7f3ba5fdbf5ff12b63
Bruno Cauet <bruno.cauet@supelec.fr>
2011-02-26 01:50:04 +0100

    [MOD] Nedded in order to remove some shittiness [work in progress, color=love]

--- apps/meps/views.py
+++ apps/meps/views.py
@@ -62,6 +62,13 @@ def mep(request, mep_id):
     score_list.sort(key = lambda k : k['value'])
     print score_list
     scores = [s['value'] for s in mep_.scores]
+    
+    #AAAWWWFFFUUUUUULLLLLLL
+    i=0
+    for score in score_list:
+        score_list[i] = {'label': score['label'], 'value': score['value'], 'couleur': score['value']/10}
+        i+=1
+
     context = {
         'mep_id': mep_id,
         'mep': mep_,
@@ -69,7 +76,7 @@ def mep(request, mep_id):
         'visible_count': len([x for x in positions if x.visible]),
         'average': sum(scores)/len(scores) if len(scores) > 0 else "",
         'score_list' : score_list,
-#        'color_index' : map(lambda  i : int(float(i))/10 , scores),   ## TODO color_index
+        'color_index' : map(lambda  i : int(float(i))/10 , scores),   ## TODO color_index
         'vote_colors' : ['#ff0000', '#dd0022', '#bb0044', '#dd0022', '#bb0044', '#990066', '#770088', '#5500aa', '#3300cc', '#0000ff'],
     }
     return direct_to_template(request, 'meps/mep.html', context)
