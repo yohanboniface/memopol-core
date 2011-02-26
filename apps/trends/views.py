f77ff23467c72906ccb09ae9941c4b50c8885b72
Laurent Peuch <cortex@worlddomination.be>
2011-02-26 21:22:05 +0100

    [mod] some modifications of the test application trends

--- apps/trends/views.py
+++ apps/trends/views.py
@@ -1,21 +1,26 @@
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
-import matplotlib
-matplotlib.use("Agg")
-from matplotlib import pyplot
-
 from os.path import realpath
 
 from meps.models import MEP
 
 def big_mess(request):
-    mep_ = MEP.view('meps/by_id', key="AlexanderAlvaro").first()
-    score_list = mep_.scores
-    pyplot.plot([x['value'] for x in score_list])
-    print [score_list]
-    pyplot.savefig(realpath("./%simg/trends/a.png" % settings.MEDIA_URL), format="png")
+    meps = MEP.view('meps/by_name')
+    #positions = Position.objects.filter(mep_id=mep_id)
+    for mep_ in meps:
+        import matplotlib
+        matplotlib.use("Agg")
+        from matplotlib import pyplot
+
+        score_list = mep_.scores
+        pyplot.plot([x['value'] for x in score_list])
+        pyplot.xlabel("%s %s" % (mep_.last, mep_.first))
+        print [score_list]
+        pyplot.savefig(realpath("./%simg/trends/%s.png" % (settings.MEDIA_URL, mep_.id)), format="png")
+        print mep_.id
+        pyplot.clf()
     context = {
-        'trends' : ['a'],
+        'trends' : [m.id for m in meps],
     }
     return direct_to_template(request, 'trends.html', context)
