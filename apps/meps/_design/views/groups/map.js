568323b44c3d7f9a84922ae5b3f6f00c05abff73
Laurent Peuch <cortex@worlddomination.be>
2011-03-01 05:18:47 +0100

    trailing spaces

--- apps/meps/_design/views/groups/map.js
+++ apps/meps/_design/views/groups/map.js
@@ -1,7 +1,7 @@
-function(doc) { 
-    emit(doc.infos.group.abbreviation, { 
+function(doc) {
+    emit(doc.infos.group.abbreviation, {
         code: doc.infos.group.abbreviation,
         name: doc.infos.group.name,
         count: 1
-    }); 
+    });
 }
