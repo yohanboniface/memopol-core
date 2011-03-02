4c7e78fbfb90943be8ec1844dc157637207391f7
Laurent Peuch <cortex@worlddomination.be>
2011-03-02 12:27:29 +0100

    [fix] sort french mps by last name

--- apps/mps/_design/views/all/map.js
+++ apps/mps/_design/views/all/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    emit(doc._id, {
+    emit(doc.infos.name.last, {
         'id': doc._id,
         'extid': doc.extid,
         'name': doc.infos.name.first+' '+doc.infos.name.last
