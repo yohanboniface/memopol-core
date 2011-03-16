609e1a0f069bfbef963e1b5cc461c16e8d58a913
stef <stefan.marsiske@gmail.com>
2011-03-16 03:27:39 +0100

    [enh] displaying some automatic trophies under MEPs names, icons sorely needed.

--- apps/meps/views.py
+++ apps/meps/views.py
@@ -13,7 +13,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from meps.models import MEP, Position
 
-
 def index_names(request):
     meps_by_name = MEP.view('meps/by_name')
     context = {
@@ -69,8 +68,45 @@ def score_to_color(score):
     green = score * 2.55
     return "rgb(%d, %d, 0)" % (red, green)
 
+def autoTrophies(mep):
+    mapping = { (u'Parlement europ\u00e9en',u'Pr\u00e9sident') : (12, 'President of EP', 'pep.jpg'),
+                (u'Parlement europ\u00e9en',u'Vice-Pr\u00e9sident') : (11, 'VP of EP', 'vpep.jpg'),
+                (u'Bureau du Parlement europ\u00e9en',u'Pr\u00e9sident') : (11,'President of EU Parlament Office', 'pepo.jpg'),
+                (u'Bureau du Parlement europ\u00e9en',u'Vice-Pr\u00e9sident') : (10, 'VP of EU Parlament Office', 'vpepo.jpg'),
+                (u'Bureau du Parlement europ\u00e9en',u'Membre') : (9, 'Member of EU Parlament Office', 'mepo.jpg'),
+                (u'Conf\u00e9rence des pr\u00e9sidents', u'Pr\u00e9sident') : (11, "CoP President", 'pcop.jpg'),
+                (u'Conf\u00e9rence des pr\u00e9sidents', u'Vice-pr\u00e9sident') : (9, "CoP VP", 'vpcop.jpg'),
+                (u'Conf\u00e9rence des pr\u00e9sidents', u'Membre') : (8, "CoP Member", 'mcop.jpg'),
+                (u'Conf\u00e9rence des pr\u00e9sidents des commissions', u'Pr\u00e9sident') : (7, "CoCP President", 'pcocp.jpg'),
+                (u'Conf\u00e9rence des pr\u00e9sidents des commissions', u'Vice-pr\u00e9sident') : (6, "CoCP VP", 'vpcocp.jpg') ,
+                (u'Conf\u00e9rence des pr\u00e9sidents des commissions', u'Membre') : (5, "CoCP Member", 'mcocp.jpg'),
+                }
+    res=[]
+    for fn in mep.functions:
+        try: # for Mr Lehnes record
+            m=mapping.get((fn['label'],fn['role']))
+        except TypeError:
+            m=None
+        if m:
+            res.append(m)
+            continue
+        if fn.get('abbreviation'):
+            if fn['role'].startswith(u'Pr\u00e9sident'): res.append((7, fn['abbreviation']+" President", 'presc.jpg'))
+            elif fn['role'].startswith(u'Vice-pr\u00e9sident'): res.append((5,fn['abbreviation']+" VP", 'vpc.jpg'))
+            elif fn['role'] == u'Membre': res.append( (2,fn['abbreviation']+" Member", 'mc.jpg'))
+            elif fn['role'] == u'Membre suppl\u00e9ant': res.append((1, fn['abbreviation']+" Supplement", 'sc.jpg'))
+    if mep.infos['group']['role'] in [u'Pr\u00e9sident', u'Copr\u00e9sident']:
+        res.append((12, 'President of '+mep.infos['group']['abbreviation'], 'pg.jpg'))
+    if mep.infos['group']['role'].startswith(u'Vice-pr\u00e9sident'):
+        res.append((10, 'VP of '+mep.infos['group']['abbreviation'], 'vpg.jpg'))
+    for op in mep.opinions:
+        if op['url'] == 'http://www.laquadrature.net/wiki/Written_Declaration_12/2010_signatories_list':
+            res.append((5, 'signed WD12', 'wd12.jpg'))
+    return [(x[1], x[2]) for x in sorted(res, reverse=True)]
+
 def mep(request, mep_id):
     mep_ = MEP.get(mep_id)
+    mep_['achievements']=autoTrophies(mep_)
     positions = Position.objects.filter(mep_id=mep_id)
     score_list = mep_.scores
     for score in score_list:
