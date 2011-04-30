599461e7946c4c025c4e50f4c2c79f8b9cec1127
Laurent Peuch <cortex@worlddomination.be>
2011-04-30 23:57:10 +0200

    [enh] add some models to the admin interface

--- apps/meps/admin.py
+++ apps/meps/admin.py
@@ -1,11 +1,14 @@
-from models import Committe, Deleguation, Country, Group, Opinion, Mep, Party, WebSite
+from models import Committe, Deleguation, Country, Group, Opinion, Mep, Party, WebSite, CommitteRole, DeleguationRole, OpinionMep
 from django.contrib import admin
 
 admin.site.register(Mep)
 admin.site.register(Committe)
+admin.site.register(CommitteRole)
 admin.site.register(Deleguation)
+admin.site.register(DeleguationRole)
 admin.site.register(Country)
 admin.site.register(Group)
 admin.site.register(Party)
 admin.site.register(Opinion)
+admin.site.register(OpinionMep)
 admin.site.register(WebSite)
