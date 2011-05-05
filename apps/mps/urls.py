34ddf9e4afe37255748d5b21fb00a0570f1f46a5
Laurent Peuch <cortex@worlddomination.be>
2011-05-05 02:10:13 +0200

    [enh] french mp list by name works again (but Group object will need more work)

--- apps/mps/urls.py
+++ apps/mps/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import list_detail
 
 from mps import views
+from mps.models import MP
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', list_detail.object_list, {'queryset': MP.objects.filter(active=True)}, name='index'),
     url(r'^(?P<mp_id>[a-zA-Z]+)/$', views.detail, name='detail'),
 )
