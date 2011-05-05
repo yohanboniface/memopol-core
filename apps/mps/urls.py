61822b0925040e1c3f91ec78bf77028c9b2eeb62
Laurent Peuch <cortex@worlddomination.be>
2011-05-05 13:38:50 +0200

    [fix] correct mp detail list url name

--- apps/mps/urls.py
+++ apps/mps/urls.py
@@ -5,6 +5,6 @@ from mps.models import MP, Group
 
 urlpatterns = patterns('',
     url(r'^$', list_detail.object_list, {'queryset': MP.objects.filter(active=True)}, name='index'),
-    url(r'^mp/(?P<object_id>[a-zA-Z]+)/$', list_detail.object_detail, {'queryset': MP.objects.all(), 'template_object_name': 'mp'}, name='detail'),
+    url(r'^mp/(?P<object_id>[a-zA-Z]+)/$', list_detail.object_detail, {'queryset': MP.objects.all(), 'template_object_name': 'mp'}, name='mp'),
     url(r'^groups/$', list_detail.object_list, {'queryset': Group.objects.all()}, name='index_groups'),
 )
