ef07dc4b7f90af02e840f5ee7f32573edfa4c8df
Laurent Peuch <cortex@worlddomination.be>
2011-05-05 23:43:44 +0200

    [enh] vote listing works again

--- apps/votes/urls.py
+++ apps/votes/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import list_detail
 
 from votes import views
+from votes.models import Vote
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', list_detail.object_list, {'queryset': Vote.objects.all()}, name='index'),
     url(r'^(?P<vote_name>[a-zA-Z/-_]+)/$', views.detail, name='detail'),
 )
