2896cf9ec02f4105ef4541c8ae69d38c427aedc2
Laurent Peuch <cortex@worlddomination.be>
2011-05-05 03:31:27 +0200

    [enh] french mp detail page work again

--- apps/mps/urls.py
+++ apps/mps/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import list_detail
 
-from mps import views
 from mps.models import MP
 
 urlpatterns = patterns('',
     url(r'^$', list_detail.object_list, {'queryset': MP.objects.filter(active=True)}, name='index'),
-    url(r'^(?P<mp_id>[a-zA-Z]+)/$', views.detail, name='detail'),
+    url(r'^mp/(?P<object_id>[a-zA-Z]+)/$', list_detail.object_detail, {'queryset': MP.objects.all(), 'template_object_name': 'mp'}, name='detail'),
 )
