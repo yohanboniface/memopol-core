6ea1e9d66f260906b8a43daf5950bb11bc810f9f
Olivier Le Thanh Duong <olivier@lethanh.be>
2011-05-08 16:04:16 +0200

    [mod] Rename vote in proposal, result in vote

--- apps/votes/models.py
+++ apps/votes/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from meps.models import MEP
 
-class Vote(models.Model):
+class Proposal(models.Model):
     id = models.CharField(max_length=63, primary_key=True)
     title = models.CharField(max_length=255, unique=True)
 
-class SubVote(models.Model):
+class SubProposal(models.Model):
     datetime = models.DateTimeField()
     subject = models.CharField(max_length=255)
     part = models.CharField(max_length=255)
     description = models.CharField(max_length=511)
     weight = models.IntegerField(null=True)
-    vote = models.ForeignKey(Vote)
+    vote = models.ForeignKey(Proposal)
     recommendation = models.CharField(max_length=15, choices=((u'against', u'against'), (u'for', u'for')), null=True)
 
-class Result(models.Model):
+class Vote(models.Model):
     choice = models.CharField(max_length=15, choices=((u'for', u'for'), (u'against', u'against'), (u'abstention', u'abstention')))
     name = models.CharField(max_length=127)
     mep = models.ForeignKey(MEP)
