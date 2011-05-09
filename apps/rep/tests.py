ca1f829d1a27e29911c46c44a49903542ca45df2
Laurent Peuch <cortex@worlddomination.be>
2011-05-09 15:36:59 +0200

    [mod] remove old useless apps/rep/ folder

--- apps/rep/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from django.core.urlresolvers import reverse
-
-class ViewsTest(TestCase):
-    """
-    Check basic context of indexes' views.
-    """
-    def setUp(self):
-        self.client = Client()
