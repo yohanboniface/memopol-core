839ab4c134c198dc5e8bad2be6642b5736c4f66b
stef <stefan.marsiske@gmail.com>
2011-05-19 01:52:05 +0200

    [fix] better vote model, improved listing of vote recommendation details.

--- apps/votes/models.py
+++ apps/votes/models.py
@@ -5,17 +5,17 @@ class Proposal(models.Model):
     id = models.CharField(max_length=63, primary_key=True)
     title = models.CharField(max_length=255, unique=True)
 
-class SubProposal(models.Model):
+class Recommendation(models.Model):
     datetime = models.DateTimeField()
     subject = models.CharField(max_length=255)
     part = models.CharField(max_length=255)
     description = models.CharField(max_length=511)
     weight = models.IntegerField(null=True)
-    vote = models.ForeignKey(Proposal)
+    proposal = models.ForeignKey(Proposal)
     recommendation = models.CharField(max_length=15, choices=((u'against', u'against'), (u'for', u'for')), null=True)
 
 class Vote(models.Model):
     choice = models.CharField(max_length=15, choices=((u'for', u'for'), (u'against', u'against'), (u'abstention', u'abstention')))
     name = models.CharField(max_length=127)
-    sub_proposal = models.ForeignKey(SubProposal, related_name="sub_proposal")
+    recommendation = models.ForeignKey(Recommendation)
     representative = models.ForeignKey(Representative)
