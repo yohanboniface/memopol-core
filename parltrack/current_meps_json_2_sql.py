df5936e296eb0fd29045d482d33331a0fcbb3b8f
Laurent Peuch <cortex@worlddomination.be>
2011-06-26 22:22:37 +0200

    [mod] change architecture of current_meps_json_2_sql.py to manage only one mep at the time and create procedure structure

--- parltrack/current_meps_json_2_sql.py
+++ parltrack/current_meps_json_2_sql.py
@@ -11,17 +11,29 @@ from meps.models import MEP
 
 current_meps = "meps.json"
 
+def clean_existant_data(mep):
+    pass
+
+def create_mep(mep_json):
+    pass
+
+def manage_mep(mep, mep_json):
+    pass
+
 if __name__ == "__main__":
     print "load json"
-    meps = json.load(open(current_meps, "r"))
-    new = 0
-    to_update = 0
-    for mep in meps["meps"]:
-        in_db_mep = MEP.objects.filter(ep_id=mep["UserID"])
-        if not in_db_mep:
-            new += 1
-        else:
-            to_update += 1
-    print "%i new meps, %i meps to update" % (new, to_update)
+    #meps = json.load(open(current_meps, "r"))
+    #new = 0
+    #to_update = 0
+    #for mep_json in meps["meps"]:
+    mep_json = json.load(open("one_mep.json", "r"))
+    in_db_mep = MEP.objects.filter(ep_id=mep_json["UserID"])
+    if in_db_mep:
+        mep = in_db_mep[0]
+        clean_existant_data(mep)
+    else:
+        mep = create_mep(mep_json)
+    manage_mep(mep, mep_json)
+    #print "%i new meps, %i meps to update" % (new, to_update)
 
 # vim:set shiftwidth=4 tabstop=4 expandtab:
