15305f91b515f15e68e11b7fc2b9e44f174763ca
Laurent Peuch <cortex@worlddomination.be>
2011-06-28 05:58:53 +0200

    [enh] let's go for all the meps now, bugs ahead git add -p

--- parltrack/current_meps_json_2_sql.py
+++ parltrack/current_meps_json_2_sql.py
@@ -125,19 +125,16 @@ def manage_mep(mep, mep_json):
 
 if __name__ == "__main__":
     print "load json"
-    #meps = json.load(open(current_meps, "r"))
-    #new = 0
-    #to_update = 0
-    #for mep_json in meps["meps"]:
-    mep_json = json.load(open("another_mep.json", "r"))
-    in_db_mep = MEP.objects.filter(ep_id=mep_json["UserID"])
-    if in_db_mep:
-        mep = in_db_mep[0]
-        clean_existant_data(mep)
-        manage_mep(mep, mep_json)
-    else:
-        mep = create_mep(mep_json)
-    #print "%i new meps, %i meps to update" % (new, to_update)
+    meps = json.load(open(current_meps, "r"))
+    for mep_json in meps["meps"]:
+        print mep_json["Name"]["full"]
+        in_db_mep = MEP.objects.filter(ep_id=mep_json["UserID"])
+        if in_db_mep:
+            mep = in_db_mep[0]
+            clean_existant_data(mep)
+            manage_mep(mep, mep_json)
+        else:
+            mep = create_mep(mep_json)
 
 # TODO
 # need to check all the existant building and to remove the empty one
