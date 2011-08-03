99777b6a15af42150980a28c0eef2f4727a846c3
Gael Pasgrimaud <gael@gawel.org>
2011-08-03 15:00:08 +0200

    [mod] return '-' if representative don't have url

--- apps/reps/models.py
+++ apps/reps/models.py
@@ -78,7 +78,7 @@ class WebSite(models.Model):
     representative = models.ForeignKey(Representative)
 
     def __unicode__(self):
-        return self.url
+        return self.url or u'-'
 
 class OpinionREP(models.Model):
     representative = models.ForeignKey(Representative)
