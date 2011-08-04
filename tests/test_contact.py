4d033247d33a1d6e402f3842085ad0f708a5d9a1
Gael Pasgrimaud <gael@gawel.org>
2011-08-04 23:31:51 +0200

    [enh] add contact form. fix #127

---
+++ tests/test_contact.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from tests import *
+
+class TestContact(TestCase):
+
+    def test_contact(self):
+        """test comments"""
+        resp = self.app.get('/contact/')
+        form = resp.forms['contact']
+        form['name'] = 'Un Garage'
+        form['email'] = 'garage@lqdn.fr'
+        resp = form.submit()
+        resp.mustcontain('<li>This field is required.</li>')
+
+        form = resp.forms['contact']
+        form['body'] = 'ACTA sucks'
+        resp = form.submit()
+
+        self.mail.mustcontain(
+            'From: memopol@lqdn.fr',
+            'To: contact@lqdn.fr',
+            'Subject: [Memopol Contact] from Un Garage',
+            'Un Garage - garage@lqdn.fr',
+            '==========',
+            'ACTA sucks',
+            )
