a10400737fbe43fb24f2ba463230ab4c249fdb6f
Gael Pasgrimaud <gael@gawel.org>
2011-08-04 22:01:39 +0200

    more tests on comment

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from tests import *
+from uuid import uuid4
 
 class TestComments(TestCase):
 
     def test_meps_comments(self):
-        """test 10 first meps"""
+        """test comments"""
         resp = self.app.get('/europe/parliament/names/')
         l = pq(resp.pyquery('a[href^="/europe/parliament/deputy/"]')[0]).attr.href
         resp = self.app.get(l)
@@ -20,4 +21,28 @@ class TestComments(TestCase):
         assert resp.status_int == 302, resp
         resp = resp.follow()
         resp.mustcontain('<h1>Thank you for your comment.</h1>')
+
+class TestCommentsModeration(UserTestCase):
+
+    def test_meps_comments(self):
+        """test comments"""
+        resp = self.app.get('/europe/parliament/names/')
+        l = pq(resp.pyquery('a[href^="/europe/parliament/deputy/"]')[0]).attr.href
+        resp = self.app.get(l)
+        self.assert_(len(resp.forms)==1, resp.forms)
+        form = resp.form # comment form is the only one
+
+        # user dont need name and email
+        form['comment'] = 'ACTA sucks'
+        resp = form.submit()
+        assert resp.status_int == 302, resp
+        resp = resp.follow()
+        resp.mustcontain('<h1>Thank you for your comment.</h1>')
+
+        # login as staff user
+        self.set_user(is_staff=True)
+        resp = self.app.get('/admin/')
+        resp.mustcontain('<a href="comments/comment/">Comments</a>')
+        resp = self.app.get('/admin/comments/comment/')
+        resp.mustcontain('<th><a href="','/">garage1</a></th>')
 
