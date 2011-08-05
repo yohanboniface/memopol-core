3713c1176af090c8de14446a37b6c19094e4e487
Laurent Peuch <cortex@worlddomination.be>
2011-08-05 20:07:05 +0200

    [mod] blank line, pep8

--- apps/mps/models.py
+++ apps/mps/models.py
@@ -8,6 +8,7 @@ class Function(models.Model):
     def __unicode__(self):
         return u'%s: %s' % (self.type, self.title and self.title[:15]+'...' or '-')
 
+
 class Department(models.Model):
     name = models.CharField(max_length=255, unique=True)
     number = models.CharField(max_length=3, primary_key=True)
@@ -21,6 +22,7 @@ class Department(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class Circonscription(models.Model):
     number = models.CharField(max_length=31)
     department = models.ForeignKey(Department)
@@ -28,12 +30,14 @@ class Circonscription(models.Model):
     def __unicode__(self):
         return u'%s - %s' % (self.number, self.department)
 
+
 class Canton(models.Model):
     name = models.CharField(max_length=511)
     circonscription = models.ForeignKey(Circonscription)
 
     def __unicode__(self):
         return self.name
+
 
 class Group(models.Model):
     abbreviation = models.CharField(max_length=31, primary_key=True)
@@ -47,6 +51,7 @@ class Group(models.Model):
 
     def __unicode__(self):
         return self.name
+
 
 class MP(Representative):
     active = models.BooleanField()
@@ -65,6 +70,7 @@ class MP(Representative):
     group = models.ForeignKey(Group)
     group_role = models.CharField(max_length=63, null=True)
 
+
 class FunctionMP(models.Model):
     mp = models.ForeignKey(MP)
     function = models.ForeignKey(Function)
@@ -80,6 +86,7 @@ class Address(models.Model):
     title = models.CharField(max_length=63, null=True)
     mp = models.ForeignKey(MP)
 
+
 class Phone(models.Model):
     type = models.CharField(max_length=5, choices=((u'phone', u'Phone'), (u'fax', u'Fax')))
     number = models.CharField(max_length=63)
@@ -87,6 +94,7 @@ class Phone(models.Model):
 
     def __unicode__(self):
         return '%s: %s' % (self.type, self.number)
+
 
 class Mandate(models.Model):
     current = models.BooleanField()
