0eaa03cb0f9efe5ca54675d8f9ece0e64e7224bb
Laurent Peuch <cortex@worlddomination.be>
2011-08-09 21:22:57 +0200

    [enh] extract code from dingues to load fixtures with frozen scheme in south

--- memopol2/utils.py
+++ memopol2/utils.py
@@ -71,3 +71,67 @@ class reify(object):
         setattr(inst, self.wrapped.__name__, val)
         return val
 
+# Code taken in dingus to fix south migration loading fixtures
+# https://github.com/garybernhardt/dingus/blob/master/dingus.py
+
+from functools import wraps
+
+def patch(object_path, new_object):
+    module_name, attribute_name = object_path.rsplit('.', 1)
+    return _Patcher(module_name, attribute_name, new_object)
+
+def _dot_lookup(thing, comp, import_path):
+    try:
+        return getattr(thing, comp)
+    except AttributeError:
+        __import__(import_path)
+        return getattr(thing, comp)
+
+def _importer(target):
+    components = target.split('.')
+    import_path = components.pop(0)
+    thing = __import__(import_path)
+
+    for comp in components:
+        import_path += ".%s" % comp
+        thing = _dot_lookup(thing, comp, import_path)
+    return thing
+
+class _Patcher:
+    def __init__(self, module_name, attribute_name, new_object):
+        self.module_name = module_name
+        self.attribute_name = attribute_name
+        self.module = _importer(self.module_name)
+        self.new_object = new_object
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def new_fn(*args, **kwargs):
+            self.patch_object()
+            try:
+                return fn(*args, **kwargs)
+            finally:
+                self.restore_object()
+        return new_fn
+
+    def __enter__(self):
+        self.patch_object()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.restore_object()
+
+    def patch_object(self):
+        self.original_object = getattr(self.module, self.attribute_name)
+        setattr(self.module, self.attribute_name, self.new_object)
+
+    def restore_object(self):
+        setattr(self.module, self.attribute_name, self.original_object)
+
+def loaddata(orm, fixture_name):
+        _get_model = lambda model_identifier: orm[model_identifier]
+
+        with patch('django.core.serializers.python._get_model', _get_model):
+            from django.core.management import call_command
+            call_command("loaddata", fixture_name)
+
+# end of code from dingus and more http://stackoverflow.com/questions/5472925/django-loading-data-from-fixture-after-backward-migration-loaddata-is-using-mod/5906258#5906258
