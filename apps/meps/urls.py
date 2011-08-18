0daf042b2d607f107516b3e7302de21cbea80468
Laurent Peuch <cortex@worlddomination.be>
2011-08-18 21:54:10 +0200

    [enh] add a slug at the end of a party listing url for SEO stuff

--- apps/meps/urls.py
+++ apps/meps/urls.py
@@ -1,11 +1,23 @@
+from django.template.defaultfilters import slugify
 from django.conf.urls.defaults import patterns, url
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView
+from django.http import HttpResponseRedirect
 from memopol2 import utils
 
 from meps.models import Country, Group, Committee, Delegation, Organization, Building
 from reps.models import Party
 
 from views import BuildingDetailView, MEPView, MEPsFromView, MEPList
+
+class PartyView(MEPsFromView):
+    model=Party
+    hidden_fields=['party']
+
+    def render_to_response(self, context):
+        if self.kwargs['slugified_name'] != slugify(self.object.name):
+            return HttpResponseRedirect(reverse('meps:index_by_party', args=[self.object.id, slugify(self.object.name)]))
+        return MEPsFromView.render_to_response(self, context)
 
 urlpatterns = patterns('meps.views',
     # those view are *very* expansive. we cache them in RAM for a week
@@ -24,7 +36,7 @@ urlpatterns = patterns('meps.views',
     url(r'^delegation/$', ListView.as_view(model=Delegation), name='index_delegations'),
     url(r'^delegation/(?P<pk>[0-9]+)/$', MEPsFromView.as_view(model=Delegation), name='index_by_delegation'),
     url(r'^party/$', ListView.as_view(model=Party), name='index_parties'),
-    url(r'^party/(?P<pk>[0-9]+)/$', MEPsFromView.as_view(model=Party, hidden_fields=['party']),  name='index_by_party'),
+    url(r'^party/(?P<pk>[0-9]+)-(?P<slugified_name>[0-9a-z\-]*)/$', PartyView.as_view(),  name='index_by_party'),
     url(r'^floor/$', ListView.as_view(queryset=Building.objects.order_by('postcode')), name='index_floor'),
     url(r'^floor/brussels/(?P<pk>\w+)/(?P<floor>\w+)/$', BuildingDetailView.as_view(), name='bxl_floor'),
     url(r'^floor/strasbourg/(?P<pk>\w+)/(?P<floor>\w+)/$', BuildingDetailView.as_view(), name='stg_floor'),
