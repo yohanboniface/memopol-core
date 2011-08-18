b714a98121efc674b25de0320bc4fdcaae7011b5
Laurent Peuch <cortex@worlddomination.be>
2011-08-18 17:27:19 +0200

    [enh] move mep listing by recommendaiton choice to CBGV

--- apps/votes/urls.py
+++ apps/votes/urls.py
@@ -20,31 +20,40 @@ def proposal_rep(request, proposal_id, mep_id):
     context = {'representative': representative, 'proposal': proposal, 'votes': votes}
     return render_to_response('votes/per_rep.html', context, context_instance=RequestContext(request))
 
-def mep_recommendation(request, proposal_id, recommendation_id, recommendation):
-    _recommendation = get_object_or_404(Recommendation, id=recommendation_id)
-    meps = MEP.objects.filter(vote__recommendation=_recommendation,
-                              vote__choice=recommendation)
-    return render_to_response("meps/mep_list.html", {'recommendation': _recommendation, 'choice': recommendation, 'object_list' : meps, 'header_template' : 'votes/header_mep_list.html'}, context_instance=RequestContext(request))
-
 class VoteRecommendation(DetailView):
     template_name='votes/recommendation_detail.html'
+    redirect="votes:recommendation"
 
     def get_context_data(self, *args, **kwargs):
         context = super(VoteRecommendation, self).get_context_data(**kwargs)
         context['choice_listing'] = True
         context['proposal'] = self.object.proposal
+        self.redirect_args = [self.object.proposal.id, self.object.id]
         return context
 
     def render_to_response(self, context):
         if self.kwargs["proposal_id"] != self.object.proposal.id:
-            return HttpResponseRedirect(reverse("votes:recommendation", args=[self.object.proposal.id, self.object.id]))
+            return HttpResponseRedirect(reverse(self.redirect, args=self.redirect_args))
         return DetailView.render_to_response(self, context)
+
+class VoteRecommendationChoice(VoteRecommendation):
+    template_name='meps/mep_list.html'
+    redirect="votes:recommendation_choice"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VoteRecommendationChoice, self).get_context_data(**kwargs)
+        context['choice'] = self.kwargs['recommendation']
+        context['header_template'] = 'votes/header_mep_list.html'
+        context['object_list'] = MEP.objects.filter(vote__recommendation=self.object,
+                                  vote__choice=self.kwargs['recommendation'])
+        self.redirect_args += [self.kwargs['recommendation']]
+        return context
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Proposal), name='index'),
     url(r'^import/$', ListView.as_view(model=RecommendationData), name='import'),
     url(r'^import/(?P<pk>\d+)/$', DetailView.as_view(model=RecommendationData), name='import_vote'),
-    url(r'^(?P<proposal_id>[a-zA-Z/-_]+)/(?P<recommendation_id>\d+)/(?P<recommendation>\w+)/$', mep_recommendation, name='recommendation_choice'),
+    url(r'^(?P<proposal_id>[a-zA-Z/-_]+)/(?P<pk>\d+)/(?P<recommendation>\w+)/$', VoteRecommendationChoice.as_view(model=Recommendation), name='recommendation_choice'),
     url(r'^(?P<proposal_id>[a-zA-Z/-_]+)/(?P<pk>\d+)/$', VoteRecommendation.as_view(model=Recommendation), name='recommendation'),
     url(r'^(?P<proposal_id>[a-zA-Z/-_]+)/(?P<mep_id>[a-zA-Z-_]+)/$', proposal_rep, name='rep'),
     url(r'^(?P<pk>[a-zA-Z/-_]+)/$', DetailView.as_view(model=Proposal, context_object_name='vote'), name='detail'),
