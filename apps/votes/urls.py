71409fc06568a3e32c698b3408c0e2c2bedee6ec
Laurent Peuch <cortex@worlddomination.be>
2011-08-18 17:33:22 +0200

    [mod] move votes CGBV to views.py

--- apps/votes/urls.py
+++ apps/votes/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 
 from votes.models import Proposal, Vote, Recommendation, RecommendationData
 from reps.models import Representative
-from meps.models import MEP
+from views import VoteRecommendation, VoteRecommendationChoice
 
 # TODO: refactor those 3 functions, should probably be moved to generic views if possible
 def proposal_rep(request, proposal_id, mep_id):
@@ -19,35 +17,6 @@ def proposal_rep(request, proposal_id, mep_id):
              for r in proposal.recommendation_set.all()]
     context = {'representative': representative, 'proposal': proposal, 'votes': votes}
     return render_to_response('votes/per_rep.html', context, context_instance=RequestContext(request))
-
-class VoteRecommendation(DetailView):
-    template_name='votes/recommendation_detail.html'
-    redirect="votes:recommendation"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(VoteRecommendation, self).get_context_data(**kwargs)
-        context['choice_listing'] = True
-        context['proposal'] = self.object.proposal
-        self.redirect_args = [self.object.proposal.id, self.object.id]
-        return context
-
-    def render_to_response(self, context):
-        if self.kwargs["proposal_id"] != self.object.proposal.id:
-            return HttpResponseRedirect(reverse(self.redirect, args=self.redirect_args))
-        return DetailView.render_to_response(self, context)
-
-class VoteRecommendationChoice(VoteRecommendation):
-    template_name='meps/mep_list.html'
-    redirect="votes:recommendation_choice"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(VoteRecommendationChoice, self).get_context_data(**kwargs)
-        context['choice'] = self.kwargs['recommendation']
-        context['header_template'] = 'votes/header_mep_list.html'
-        context['object_list'] = MEP.objects.filter(vote__recommendation=self.object,
-                                  vote__choice=self.kwargs['recommendation'])
-        self.redirect_args += [self.kwargs['recommendation']]
-        return context
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Proposal), name='index'),
---
+++ apps/votes/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView
+
+from meps.models import MEP
+
+class VoteRecommendation(DetailView):
+    template_name='votes/recommendation_detail.html'
+    redirect="votes:recommendation"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VoteRecommendation, self).get_context_data(**kwargs)
+        context['choice_listing'] = True
+        context['proposal'] = self.object.proposal
+        self.redirect_args = [self.object.proposal.id, self.object.id]
+        return context
+
+    def render_to_response(self, context):
+        if self.kwargs["proposal_id"] != self.object.proposal.id:
+            return HttpResponseRedirect(reverse(self.redirect, args=self.redirect_args))
+        return DetailView.render_to_response(self, context)
+
+
+class VoteRecommendationChoice(VoteRecommendation):
+    template_name='meps/mep_list.html'
+    redirect="votes:recommendation_choice"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VoteRecommendationChoice, self).get_context_data(**kwargs)
+        context['choice'] = self.kwargs['recommendation']
+        context['header_template'] = 'votes/header_mep_list.html'
+        context['object_list'] = MEP.objects.filter(vote__recommendation=self.object,
+                                  vote__choice=self.kwargs['recommendation'])
+        self.redirect_args += [self.kwargs['recommendation']]
+        return context
