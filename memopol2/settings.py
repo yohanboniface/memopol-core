8c6d65cdf68c06b02daecfe1f95d3a3db7b89d6a
Cynddl <intnumerix@gmail.com>
2011-08-23 13:26:25 +0200

    [fix] Csrf deprecated issue

--- memopol2/settings.py
+++ memopol2/settings.py
@@ -79,7 +79,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.csrf.middleware.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
