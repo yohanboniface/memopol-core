cf96c243c78008b7b1e990c8d8063c5c1b1200d4
Laurent Peuch <cortex@worlddomination.be>
2011-08-24 12:20:20 +0200

    [enh] display important roles on mep detailed page at the beginning of the page

--- apps/meps/models.py
+++ apps/meps/models.py
@@ -229,6 +229,15 @@ class MEP(Representative):
     def party_tag(self):
         return dict(group=self.groupmep_set.latest('end').group)
 
+    @reify
+    def important_posts(self):
+        all_roles = [list(OrganizationMEP.objects.filter(mep=self, end__gt=date.today()))]
+        for i in (GroupMEP, CommitteeRole, DelegationRole):
+            roles = i.objects.filter(mep=self, end__gt=date.today()).exclude(role="Member").exclude(role="Substitute")
+            if roles:
+                all_roles.append(list(roles))
+        return roles
+
     class Meta:
         ordering = ['last_name']
 
@@ -240,6 +249,10 @@ class GroupMEP(models.Model):
     begin = models.DateField(null=True)
     end = models.DateField(null=True)
 
+    @reify
+    def instance(self):
+        return self.group
+
 
 class DelegationRole(models.Model):
     mep = models.ForeignKey(MEP)
@@ -247,6 +260,10 @@ class DelegationRole(models.Model):
     role = models.CharField(max_length=255)
     begin = models.DateField(null=True)
     end = models.DateField(null=True)
+
+    @reify
+    def instance(self):
+        return self.delegation
 
     def __unicode__(self):
         return u"%s : %s" % (self.mep.full_name, self.delegation)
@@ -258,6 +275,10 @@ class CommitteeRole(models.Model):
     role = models.CharField(max_length=255)
     begin = models.DateField(null=True)
     end = models.DateField(null=True)
+
+    @reify
+    def instance(self):
+        return self.committee
 
     def __unicode__(self):
         return u"%s : %s" % (self.committee.abbreviation, self.mep.full_name)
@@ -282,6 +303,10 @@ class OrganizationMEP(models.Model):
     role = models.CharField(max_length=255)
     begin = models.DateField()
     end = models.DateField()
+
+    @reify
+    def instance(self):
+        return self.organization
 
 
 class MepModerator(CommentModerator):
