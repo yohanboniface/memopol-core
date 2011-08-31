321ae37e45cae0c1d423fe2088978c7f2655d90a
stef <stefan.marsiske@gmail.com>
2011-08-31 18:57:42 +0200

    [enh] improved campaign weighting - now works \o/

--- apps/campaign/forms.py
+++ apps/campaign/forms.py
@@ -5,28 +5,28 @@ from django.utils.translation import ugettext as _
 from meps.models import GroupMEP, CommitteeRole, DelegationRole, OrganizationMEP, MEP, Committee, Group, Organization, Delegation
 
 class ScoreForm(forms.Form):
-   score = forms.IntegerField(required=True)
-   groupRole = forms.MultipleChoiceField(required=False,
-                                 choices=(('',''),)+tuple([(''.join(x['role'].lower().split()), x['role'])
-                                                           for x in GroupMEP.objects.filter(mep__active=True).values('role').distinct()]))
-   group = forms.MultipleChoiceField(required=False,
-                                     choices=(('',''),)+tuple([(''.join(x['abbreviation'].lower().split()), x['abbreviation'])
-                                                               for x in Group.objects.filter(groupmep__mep__active=True).values('abbreviation').distinct()]))
-   staffRole = forms.MultipleChoiceField(required=False,
-                                 choices=(('',''),)+tuple([(''.join(x['role'].lower().split()), x['role'])
-                                                           for x in OrganizationMEP.objects.filter(mep__active=True).values('role').distinct()]))
-   staff = forms.MultipleChoiceField(required=False,
-                                     choices=(('',''),)+tuple([(''.join(x['name'].lower().split()), x['name'])
-                                                               for x in Organization.objects.filter(organizationmep__mep__active=True).values('name').distinct()]))
-   committeeRole = forms.MultipleChoiceField(required=False,
-                                     choices=(('',''),)+tuple([(''.join(x['role'].lower().split()), x['role'])
-                                                               for x in CommitteeRole.objects.filter(mep__active=True).values('role').distinct()]))
+   weight = forms.IntegerField(required=True)
    committee = forms.MultipleChoiceField(required=False,
-                                         choices=(('',''),)+tuple([(''.join(x['name'].lower().split()), x['name'])
+                                         choices=(('',''),)+tuple([(x['name'], x['name'])
                                                                    for x in Committee.objects.filter(committeerole__mep__active=True).values('name').distinct()]))
-   delegationRole = forms.MultipleChoiceField(required=False,
-                                      choices=(('',''),)+tuple([(''.join(x['role'].lower().split()), x['role'])
-                                                                for x in DelegationRole.objects.filter(mep__active=True).values('role').distinct()]))
+   committeeRole = forms.MultipleChoiceField(required=False,
+                                             choices=(('',''),)+tuple([(x['role'], x['role'])
+                                                                       for x in CommitteeRole.objects.filter(mep__active=True).values('role').distinct()]))
+   group = forms.MultipleChoiceField(required=False,
+                                     choices=(('',''),)+tuple([(x['abbreviation'], x['abbreviation'])
+                                                               for x in Group.objects.filter(groupmep__mep__active=True).values('abbreviation').distinct()]))
+   groupRole = forms.MultipleChoiceField(required=False,
+                                         choices=(('',''),)+tuple([(x['role'], x['role'])
+                                                                   for x in GroupMEP.objects.filter(mep__active=True).values('role').distinct()]))
+   staff = forms.MultipleChoiceField(required=False,
+                                     choices=(('',''),)+tuple([(x['name'], x['name'])
+                                                               for x in Organization.objects.filter(organizationmep__mep__active=True).values('name').distinct()]))
+   staffRole = forms.MultipleChoiceField(required=False,
+                                         choices=(('',''),)+tuple([(x['role'], x['role'])
+                                                                   for x in OrganizationMEP.objects.filter(mep__active=True).values('role').distinct()]))
    delegation = forms.MultipleChoiceField(required=False,
-                                          choices=(('',''),)+tuple([(''.join(x['name'].lower().split()), x['name'])
+                                          choices=(('',''),)+tuple([(x['name'], x['name'])
                                                                     for x in Delegation.objects.filter(delegationrole__mep__active=True).values('name').distinct()]))
+   delegationRole = forms.MultipleChoiceField(required=False,
+                                              choices=(('',''),)+tuple([(x['role'], x['role'])
+                                                                        for x in DelegationRole.objects.filter(mep__active=True).values('role').distinct()]))
--- apps/campaign/views.py
+++ apps/campaign/views.py
@@ -3,12 +3,58 @@
 from models import Campaign
 from django.shortcuts import render_to_response, get_object_or_404
 from campaign.forms import ScoreForm
+from campaign.models import MEPScore
 from django.template import RequestContext
+from meps.models import MEP, OrganizationMEP
+from datetime import date
 
-def updateCampaignScores(form, pk):
-    #{'group': [u'alde'], 'committee': [], 'delegation': [], 'score': -10, 'committeeRole': [], 'groupRole': [u'chair', u'chairofthebureau', u'co-chair', u'deputychair', u'deputytreasurer', u'member', u'treasurer', u'vice-chair', u'vice-chair/memberofthebureau'], 'staffRole': [], 'delegationRole': [], 'staff': []}
-    if 'groupRole' in form.cleaned_data:
-        print form.cleaned_data['groupRole']
+def updateCampaignScores(form, pk, c):
+    #meps=[]
+    query={}
+    if form.cleaned_data['group']:
+        query['groupmep__group__abbreviation__in']=form.cleaned_data['group']
+        query['groupmep__end']=date(9999, 12, 31)
+        if form.cleaned_data['groupRole']:
+            query['groupmep__role__in']=form.cleaned_data['groupRole']
+            query['groupmep__end']=date(9999, 12, 31)
+
+    if form.cleaned_data['delegation']:
+        query['delegationrole__delegation__name__in']=form.cleaned_data['delegation']
+        query['delegationrole__end']=date(9999, 12, 31)
+        if form.cleaned_data['delegationRole']:
+            query['delegationrole__role__in']=form.cleaned_data['delegationRole']
+            query['delegationrole__end']=date(9999, 12, 31)
+
+    if form.cleaned_data['staff']:
+        #query=OrganizationMEP.objects.filter(organization__name__in=form.cleaned_data['staff'])
+        #print query.count()
+        #print '\n'.join([str((x.end,x.mep,x.organization)) for x in query]).decode('utf8')
+        #query=query.filter(end=date(9999, 12, 31))
+        #print query.count()
+        #print '\n'.join([str((x.end,x.mep,x.organization)) for x in query]).decode('utf8')
+        query['organizationmep__organization__name__in']=form.cleaned_data['staff']
+        query['organizationmep__end']=date(9999, 12, 31)
+        if form.cleaned_data['staffRole']:
+            #query=query.filter(role__in=form.cleaned_data['staffRole'])
+            query['organizationmep__role__in']=form.cleaned_data['staffRole']
+            query['organizationmep__end']=date(9999, 12, 31)
+        #meps.extend([x.mep for x in query])
+
+    if form.cleaned_data['committee']:
+        query['committeerole__committee__name__in']=form.cleaned_data['committee']
+        query['committeerole__end']=date(9999, 12, 31)
+        if form.cleaned_data['committeeRole']:
+            query['committeerole__role__in']=form.cleaned_data['committeeRole']
+            query['committeerole__end']=date(9999, 12, 31)
+
+    #print meps
+    #if meps:
+    if query:
+        for mep in MEP.objects.filter(**query).distinct():
+        #for mep in meps:
+            ms=MEPScore.objects.get_or_create(mep=mep, campaign=c)[0]
+            ms.score+=form.cleaned_data['weight']
+            ms.save()
 
 def editCampaign(request, pk):
     c=get_object_or_404(Campaign, pk=pk)
@@ -17,7 +63,7 @@ def editCampaign(request, pk):
     if not form.is_valid():
         form = ScoreForm()
     else:
-        updateCampaignScores(form, pk)
+        updateCampaignScores(form, pk, c)
     data['form']=form
     return render_to_response('campaign/edit.html',
                               data,
