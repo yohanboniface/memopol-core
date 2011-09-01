b712fff4d44cb4dcbe70b27b7270f3c993c821ff
Laurent Peuch <cortex@worlddomination.be>
2011-09-01 09:27:48 +0200

    [mod] if mep doesn't have any scores, set his total_score to None

--- apps/meps/utils.py
+++ apps/meps/utils.py
@@ -21,8 +21,8 @@ def update_total_score_of_mep(mep, proposal=Proposal, score=Score):
     proposals = proposal.objects.all()
     total = proposal.objects.aggregate(Sum('ponderation'))['ponderation__sum']
     # all the votes a mep has been involved in
-    done = [_score.proposal for _score in score.objects.filter(representative=mep.representative_ptr)]
-    total_score = sum([_score.value * _score.proposal.ponderation for _score in score.objects.filter(representative=mep.representative_ptr)])
+    done = [score.proposal for score in score.objects.filter(representative=mep.representative_ptr)]
+    total_score = sum([score.value * score.proposal.ponderation for score in score.objects.filter(representative=mep.representative_ptr)])
     # add 50 by default to votes a meps hasn't been involved in
     total_score += sum([50 * missing.ponderation for missing in proposals if missing not in done])
     mep.total_score = total_score / float(total)
