4c9a73ce22aa2bf8e295a21659eb6b75957ccb1b
Laurent Peuch <cortex@worlddomination.be>
2011-09-04 13:17:23 +0200

    [mod] remove useless import

--- apps/campaign/urls.py
+++ apps/campaign/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 from campaign import views as CampaignView
-from meps.models import Country, Group, Committee, Delegation, Organization, Building
 
 urlpatterns = patterns('campaign.views',
     url(r'^edit/(?P<pk>\w+)/$', CampaignView.editCampaign, ),
