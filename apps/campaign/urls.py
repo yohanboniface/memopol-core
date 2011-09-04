ea79f5c53e7cbcef2b3c297e1a1572adef630d84
Laurent Peuch <cortex@worlddomination.be>
2011-09-04 13:17:51 +0200

    [mod] add name= to urls of campaign

--- apps/campaign/urls.py
+++ apps/campaign/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 from campaign import views as CampaignView
 
 urlpatterns = patterns('campaign.views',
-    url(r'^edit/(?P<pk>\w+)/$', CampaignView.editCampaign, ),
-    url(r'^view/(?P<pk>\w+)/$', CampaignView.getCampaignMeps, ),
+    url(r'^edit/(?P<pk>\w+)/$', CampaignView.editCampaign, name="edit"),
+    url(r'^view/(?P<pk>\w+)/$', CampaignView.getCampaignMeps, name="view"),
 )
