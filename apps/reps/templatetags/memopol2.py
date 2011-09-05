1bedbe591e8b8b18ed39089809fbc3dff526843e
stef <stefan.marsiske@gmail.com>
2011-09-05 22:57:38 +0200

    [enh] added 'addurlparameter' to templatetags

--- apps/reps/templatetags/memopol2.py
+++ apps/reps/templatetags/memopol2.py
@@ -1,4 +1,4 @@
-from django.template import Library, Variable
+from django.template import Library, Variable, Node, resolve_variable, TemplateSyntaxError
 from django.conf import settings
 from django import template
 import random
@@ -19,3 +19,22 @@ def phone(value):
         value = value.replace(v, '')
     return value
 
+class AddParameter(Node):
+  def __init__(self, varname, value):
+    self.varname = varname
+    self.value = value
+
+  def render(self, context):
+    req = resolve_variable('request',context)
+    params = req.GET.copy()
+    params[self.varname] = self.value
+    return '%s?%s' % (req.path, params.urlencode())
+
+def addurlparameter(parser, token):
+  from re import split
+  bits = split(r'\s+', token.contents, 2)
+  if len(bits) < 2:
+    raise TemplateSyntaxError, "'%s' tag requires two arguments" % bits[0]
+  return AddParameter(bits[1],bits[2])
+
+register.tag('addurlparameter', addurlparameter)
