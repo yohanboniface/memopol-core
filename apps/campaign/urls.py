a9d35d5a49a81888fb20ac1a725fbe75256f34be
stef <stefan.marsiske@gmail.com>
2011-09-05 23:14:16 +0200

    [fix] faulty merge

--- apps/campaign/urls.py
+++ apps/campaign/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 from campaign import views as CampaignView
 
+urlpatterns = patterns('campaign.views',
     url(r'^list/$', CampaignView.getCampaigns, name="list"),
     url(r'^edit/(?P<pk>[0-9]+)/$', CampaignView.editCampaign, name="edit"),
     url(r'^view/(?P<pk>[0-9]+)/$', CampaignView.getCampaignMeps, name="view"),
