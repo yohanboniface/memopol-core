746032e5a323b5dd8c2d4f056345c0cd8787c152
Laurent Peuch <cortex@worlddomination.be>
2011-09-08 19:48:11 +0200

    [enh] ugly patch to fix the ordering of the votes listing on each mep on proposal detail page

--- apps/votes/templatetags/votes_tags.py
+++ apps/votes/templatetags/votes_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def mep_votes_list_on_proposal(context, mep, proposal):
-    context['mep_votes'] = Vote.objects.filter(representative=mep.representative_ptr, recommendation__proposal=proposal)
+    context['mep_votes'] = sorted(Vote.objects.filter(representative=mep.representative_ptr, recommendation__proposal=proposal), key=lambda x: x.recommendation.datetime)
     return ''
