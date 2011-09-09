129014ebf88ee6f443a45444b4befcb94e6b4d76
Laurent Peuch <cortex@worlddomination.be>
2011-09-09 23:18:49 +0200

    [enh] set the current mps to active with nosdeputes data

--- nosdeputes/update_mps.sh
+++ nosdeputes/update_mps.sh
@@ -5,7 +5,7 @@ export PYTHONPATH="../apps/:"$PYTHONPATH
 export DJANGO_SETTINGS_MODULE="memopol2.settings"
 
 echo "Starting the update"
-python ./update_mps.py || {echo "import json data into the database failed" && echo "end" && exit 1}
+python ./update_mps.py || ( echo "import json data into the database failed" && echo "end" && exit 1 )
 
 echo
 echo "successful end"
