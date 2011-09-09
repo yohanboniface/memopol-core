4ecd9b19058d3a8fdd5a5fe99dfe08fbe25cfaf4
Laurent Peuch <cortex@worlddomination.be>
2011-09-09 20:35:59 +0200

    [enh] add first files for importation of mps from nosdeputes

---
+++ nosdeputes/update_mps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONPATH="../:"$PYTHONPATH
+export PYTHONPATH="../apps/:"$PYTHONPATH
+export DJANGO_SETTINGS_MODULE="memopol2.settings"
+
+echo "Starting the update"
+python ./update_mps.py || {echo "import json data into the database failed" && echo "end" && exit 1}
+
+echo
+echo "successful end"
