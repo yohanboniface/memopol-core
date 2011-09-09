99bd1d9ea46d5ce2115dcf2925f89ccd25880fa3
Laurent Peuch <cortex@worlddomination.be>
2011-09-09 23:18:49 +0200

    [enh] set the current mps to active with nosdeputes data

--- nosdeputes/update_mps.py
+++ nosdeputes/update_mps.py
@@ -1,2 +1,31 @@
+import sys
+from json import load
+from urllib2 import urlopen
+
+from mps.models import MP
+
 if __name__ == "__main__":
-    pass
+    mps = load(urlopen("http://www.nosdeputes.fr/deputes/json"))
+
+    print "Setting all mps to unactive"
+    a = 0
+    total = MP.objects.count()
+    for mp in MP.objects.all():
+        a += 1
+        mp.active = False
+        mp.save()
+        sys.stdout.write("%i/%i\r" % (a, total))
+        sys.stdout.flush()
+    sys.stdout.write("\n")
+
+    a = 0
+    for depute in mps["deputes"]:
+        a += 1
+        mp = load(urlopen(depute["depute"]["api_url"]))["depute"]
+        print a, "-", mp["nom"]
+        _mp = MP.objects.filter(an_id=mp["url_an"].split("/")[-1].split(".")[0])
+        if _mp:
+            _mp = _mp[0]
+            if not depute["depute"].get("ancien_depute"):
+                _mp.active = True
+            _mp.save()
