d6f84febd011a4a873f85334b5dd1655cb8a2b30
bouska <bouska@fedoraproject.net>
2011-09-12 16:19:57 +0200

    [mod] Simplification of the group_proposal_score_stacked()' legend()

--- apps/trends/views.py
+++ apps/trends/views.py
@@ -463,11 +463,10 @@ def group_proposal_score_stacked(request, proposal_id):
         limit=0
         for group in proposal.groups:
             meps = group.mep_set.filter(groupmep__end__gte=proposal.date, groupmep__begin__lte=proposal.date, score__proposal=proposal, score__value__lt=score_range + 10 if score_range != 90 else 101, score__value__gte=score_range).distinct().count()
-            group_bar[group.abbreviation] = pyplot.bar(score_range/10 + 0.1, meps, width=0.8, bottom=limit, color=group_color.get(group.abbreviation, '#FFFFFF'))
+            group_bar[group.abbreviation] = pyplot.bar(score_range/10 + 0.1, meps, width=0.8, bottom=limit, color=group_color.get(group.abbreviation, '#FFFFFF'), label='%s' % group.abbreviation)
             limit+=meps
 
-    a, b = zip(*group_bar.items())
-    pyplot.legend(list(b), list(a), 'best', shadow=False)
+    pyplot.legend()
     pyplot.title("Score repartition for groups on %s" % proposal.short_name if proposal.short_name else proposal.title)
     pyplot.xticks(range(11), range(0, 110, 10))
     pyplot.xlabel("Score on vote (range 10 by 10)")
