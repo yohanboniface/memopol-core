9ca6ae7ba432d477179055a2680e592226efbf0c
Laurent Peuch <cortex@worlddomination.be>
2011-09-15 07:12:15 +0200

    [mod] refactor a bit the mp sync script

--- nosdeputes/update_mps.py
+++ nosdeputes/update_mps.py
@@ -7,9 +7,21 @@ from memopol2.utils import get_or_create
 from reps.models import Email, WebSite
 from mps.models import MP
 
-if __name__ == "__main__":
-    mps = load(urlopen("http://www.nosdeputes.fr/deputes/json"))
+def update_personal_informations(_mp, mp):
+    _mp.full_name = mp["nom"]
+    _mp.last_name = mp["nom_de_famille"]
+    _mp.an_webpage = mp["url_an"]
+    _mp.profession = mp["profession"]
 
+def get_new_websites(mp, _mp):
+    if mp["site_web"]:
+        get_or_create(WebSite, url=mp["site_web"], representative=_mp.representative_ptr)
+
+def get_new_emails(mp, _mp):
+    for email in mp["emails"]:
+        get_or_create(Email, email=email["email"], representative=_mp.representative_ptr)
+
+def set_mps_unactives():
     print "Setting all mps to unactive"
     a = 0
     total = MP.objects.count()
@@ -21,6 +33,11 @@ if __name__ == "__main__":
         sys.stdout.flush()
     sys.stdout.write("\n")
 
+if __name__ == "__main__":
+    mps = load(urlopen("http://www.nosdeputes.fr/deputes/json"))
+
+    set_mps_unactives()
+
     a = 0
     for depute in mps["deputes"]:
         a += 1
@@ -31,12 +48,7 @@ if __name__ == "__main__":
             _mp = _mp[0]
             if not depute["depute"].get("ancien_depute"):
                 _mp.active = True
-            _mp.full_name = mp["nom"]
-            _mp.last_name = mp["nom_de_famille"]
-            _mp.an_webpage = mp["url_an"]
-            _mp.profession = mp["profession"]
-            for email in mp["emails"]:
-                get_or_create(Email, email=email["email"], representative=_mp.representative_ptr)
-            if mp["site_web"]:
-                get_or_create(WebSite, url=mp["site_web"], representative=_mp.representative_ptr)
+            update_personal_informations(_mp, mp)
+            get_new_emails(mp, _mp)
+            get_new_websites(mp, _mp)
             _mp.save()
