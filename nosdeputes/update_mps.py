7c904fd6902a19d2b89edc1389d9129b685e5bda
Laurent Peuch <cortex@worlddomination.be>
2011-09-21 11:47:21 +0200

    [mod] testing for groups with sync with nosdeputes

--- nosdeputes/update_mps.py
+++ nosdeputes/update_mps.py
@@ -6,7 +6,7 @@ from urllib2 import urlopen, HTTPError
 from memopol2.utils import get_or_create
 
 from reps.models import Email, WebSite
-from mps.models import MP
+from mps.models import MP, Group
 
 def update_personal_informations(_mp, mp):
     _mp.full_name = mp["nom"]
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     mps = load(urlopen("http://www.nosdeputes.fr/deputes/json"))
 
     set_mps_unactives()
+
     a = 0
     for depute in mps["deputes"]:
         a += 1
@@ -53,20 +54,20 @@ if __name__ == "__main__":
                 print "Go repport the bug on irc.freenode.net#regardscitoyens"
                 sys.exit(1)
         print a, "-", mp["nom"]
+        #mp = load(open("test"))["depute"]
         _mp = MP.objects.filter(an_id=mp["url_an"].split("/")[-1].split(".")[0])
-        if _mp:
-            _mp = _mp[0]
-            if not depute["depute"].get("ancien_depute"):
-                _mp.active = True
-            update_personal_informations(_mp, mp)
-            get_new_emails(mp, _mp)
-            get_new_websites(mp, _mp)
-            _mp.full_name = mp["nom"]
-            _mp.last_name = mp["nom_de_famille"]
-            _mp.an_webpage = mp["url_an"]
-            _mp.profession = mp["profession"]
-            for email in mp["emails"]:
-                get_or_create(Email, email=email["email"], representative=_mp.representative_ptr)
-            if mp["site_web"]:
-                get_or_create(WebSite, url=mp["site_web"], representative=_mp.representative_ptr)
-            _mp.save()
+        try:
+            if _mp:
+                _mp = _mp[0]
+                if not depute["depute"].get("ancien_depute"):
+                    _mp.active = True
+                update_personal_informations(_mp, mp)
+                get_new_emails(mp, _mp)
+                get_new_websites(mp, _mp)
+                print mp.get("groupe")
+                #if mp["groupe_sigle"] and mp["groupe_sigle"] != "NI":
+                    #Group.objects.get(abbreviation=mp["groupe_sigle"])
+                _mp.save()
+        except Exception, e:
+            print depute["depute"]["api_url"]
+            raise e
