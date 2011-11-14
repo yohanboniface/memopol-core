3b83d314f842318e896c4a9506733a8da511c683
Sebastien Duthil <duthils@free.fr>
2011-11-14 01:15:16 +0100

    [evol] Bug #343

--- apps/votes/templatetags/votes_tags.py
+++ apps/votes/templatetags/votes_tags.py
@@ -12,3 +12,10 @@ def mep_votes_list_on_proposal(context, mep, proposal):
 def mep_score_on_vote(context, mep, proposal):
     context['mep_score'] = Score.objects.get(representative=mep, proposal=proposal)
     return ''
+
+@register.simple_tag(takes_context=True)
+def significant_votes(context, subvote):
+    ret = Vote.objects.filter(recommendation=subvote)
+    ret = ret.exclude(choice='absent').exclude(choice='abstention')
+    context['significant_votes'] = ret
+    return ''
