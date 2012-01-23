913ae4feb556dab97142051948e32777a41a100c
Laurent Peuch <cortex@worlddomination.be>
2012-01-23 05:30:57 +0100

    [fix] remove date parameter in queries to get role of meps in various stuff in the templatetags

--- apps/meps/templatetags/meps_role.py
+++ apps/meps/templatetags/meps_role.py
@@ -6,12 +6,12 @@ register = template.Library()
 
 @register.simple_tag
 def current_organization_role(mep, organization):
-    return OrganizationMEP.objects.get(mep=mep, organization=organization, end=date(9999, 12, 31)).role
+    return OrganizationMEP.objects.get(mep=mep, organization=organization).role
 
 @register.simple_tag
 def current_committee_role(mep, committee):
-    return CommitteeRole.objects.get(mep=mep, committee=committee, end=date(9999, 12, 31)).role
+    return CommitteeRole.objects.get(mep=mep, committee=committee).role
 
 @register.simple_tag
 def current_delegation_role(mep, delegation):
-    return DelegationRole.objects.get(mep=mep, delegation=delegation, end=date(9999, 12, 31)).role
+    return DelegationRole.objects.get(mep=mep, delegation=delegation).role
