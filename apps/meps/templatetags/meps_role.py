c776e91ed8ae2e921eabfb5f6c566482e1c9541a
Gael Pasgrimaud <gael@gawel.org>
2012-03-25 18:42:21 +0200

    never fail when a mep has more than one role. dont show role if mep is a 'Member'

--- apps/meps/templatetags/meps_role.py
+++ apps/meps/templatetags/meps_role.py
@@ -3,14 +3,23 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def current_organization_role(mep, organization):
-    return OrganizationMEP.objects.get(mep=mep, organization=organization).role
+    roles = OrganizationMEP.objects.filter(mep=mep, organization=organization)
+    roles = sorted([role.role for role in roles if role.role != 'Member'])
+    return roles and roles[0] or ''
+
 
 @register.simple_tag
 def current_committee_role(mep, committee):
-    return CommitteeRole.objects.get(mep=mep, committee=committee).role
+    roles = CommitteeRole.objects.filter(mep=mep, committee=committee)
+    roles = sorted([role.role for role in roles if role.role != 'Member'])
+    return roles and roles[0] or ''
+
 
 @register.simple_tag
 def current_delegation_role(mep, delegation):
-    return DelegationRole.objects.get(mep=mep, delegation=delegation).role
+    roles = DelegationRole.objects.filter(mep=mep, delegation=delegation)
+    roles = sorted([role.role for role in roles if role.role != 'Member'])
+    return roles and roles[0] or ''
