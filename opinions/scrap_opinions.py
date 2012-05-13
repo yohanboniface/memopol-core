f2ad778ae52d784c1cde0ffdeaa3b4bf417f3aba
Laurent Peuch <cortex@worlddomination.be>
2012-05-13 02:37:23 +0200

    [fix] clean date data from opinions

--- opinions/scrap_opinions.py
+++ opinions/scrap_opinions.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 from os import mkdir
 from os.path import exists
+from re import search
 from BeautifulSoup import BeautifulSoup
 from urllib import urlopen
 from json import dumps
+from dateutil.parser import parse
+
+TO_IGNORE = [
+    "[date inconnue]",
+    u"[Premier trimestre 2010] Européana (librairie numérique)",
+    "Internet",
+    u"[Date inconnue] Libertés publiques, libertés fondamentales",
+    u"[date inconnue probablement fin 2009] Internet, vente de médicaments",
+    u"contrefaçon de médicaments",
+    "[date inconnue]",
+    u"[Premier trimestre 2010] Européana (librairie numérique)",
+    u"[date inconnue] Wikileaks, Internet",
+    u"Paquet télécom, avant le vote",
+]
+
 
 def read_or_dl(url, name):
     if exists('dumps/%s' % name):
@@ -29,6 +45,25 @@ def handle_reps_set(set_name, section):
         else:
             opinion['title'] = soup.find('div', id='bodyContent').h5.text
         opinion['date'] = soup.find('div', id='bodyContent').h5.span.contents[0].strip()
+        if " " in opinion["date"]:
+            guess_date = search("\d\d/\d\d/\d\d\d\d", opinion["date"])
+            if guess_date is not None:
+                opinion["date"] = guess_date.group()
+            elif opinion["date"] in TO_IGNORE:
+                opinion["date"] = None
+            else:
+                print opinion["date"]
+                from ipdb import set_trace; set_trace()
+        if opinion["date"] in TO_IGNORE:
+            opinion["date"] = None
+        elif opinion["date"] == "0504/2011":
+            opinion["date"] = "05/04/2011"
+        try:
+            if opinion["date"] is not None:
+                parse(opinion["date"])
+        except ValueError:
+            print opinion["date"]
+            from ipdb import set_trace; set_trace()
         opinion['mep'] = i
         opinion['section'] = section
         opinion['body'] = []
@@ -49,6 +84,33 @@ def handle_reps_set(set_name, section):
                 opinion['mep'] = i
                 opinion['section'] = section
                 opinion['date'] = x.span.contents[0].strip()
+                if " " in opinion["date"]:
+                    guess_date = search("\d\d/\d\d/\d\d\d\d", opinion["date"])
+                    if guess_date is not None:
+                        opinion["date"] = guess_date.group()
+                    elif opinion["date"] == "Mai-Juin 2011":
+                        opinion["date"] = "25/05/2011"
+                    elif opinion["date"] == "04/2010 ACTA":
+                        opinion["date"] = "01/04/2010"
+                    elif opinion["date"] in ["10/2010 ACTA", "10/2010 ACTA/Hadopi"]:
+                        opinion["date"] = "01/10/2010"
+                    elif opinion["date"] == "12/2010 Wikileaks":
+                        opinion["date"] = "01/12/2010"
+                    elif opinion["date"] == u"02/2011 Internet, pédophilie":
+                        opinion["date"] = "01/02/2011"
+                    elif opinion["date"] in TO_IGNORE:
+                        opinion["date"] = None
+                    else:
+                        print opinion["date"]
+                        from ipdb import set_trace; set_trace()
+                if opinion["date"] in TO_IGNORE:
+                    opinion["date"] = None
+                try:
+                    if opinion["date"] is not None:
+                        parse(opinion["date"])
+                except ValueError:
+                    print opinion["date"]
+                    from ipdb import set_trace; set_trace()
                 opinion['body'] = []
             else:
                 opinion['body'] += [unicode(x.text if hasattr(x, 'text') else x)]
