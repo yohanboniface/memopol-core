6fd3a0542172d66d11885588d518d676659da638
Laurent Peuch <cortex@worlddomination.be>
2012-06-01 08:57:20 +0200

    [enh] continu prototyping on generic query interface, now can all/remove dynamically filters from a <select>

--- memopol2/static/js/generic_queries.js
+++ memopol2/static/js/generic_queries.js
@@ -6,7 +6,7 @@ jQuery.noConflict();
         var args = [];
         $("div.filter").each(function()
             {
-                var key = $(this).find(".key").val();
+                var key = $(this).find(".key").attr("id");
                 var value = $(this).find(".value").val();
                 if (key && value)
                 {
@@ -26,22 +26,72 @@ jQuery.noConflict();
         });
     };
 
+    var update_filters_list = function() {
+
+        $("select#filter_choice").find("option").each(function() {
+            console.debug("label#" + $(this).attr("id"));
+            console.debug($("label#" + $(this).attr("id")));
+            if ($("label#" + $(this).attr("id")).length) {
+                $(this).removeClass("visible");
+                $(this).addClass("not_visible")
+            } else {
+                $(this).removeClass("not_visible");
+                $(this).addClass("visible");
+            }
+        });
+
+        if ($("option.visible").length) {
+            $("select#filter_choice").val($("option.visible").first().attr("id"));
+        } else {
+            $("div#adders").hide()
+        }
+    };
+
     $("button#apply_filter").click(function(event) {
         update_query();
     });
 
-    $("button.remove").click(function(event) {
+    var button_remove = function(event) {
+        console.debug("remove !");
         $(this).parent().remove();
-    });
+        $("div#adders").show();
+        update_filters_list();
+    }
+
+    $("button.remove").click(button_remove);
 
     $("button#add").click(function(event) {
-        $("div#filter_list").append('<div class="filter"><input class="key" value=""> <input class="value" value=""> <button class="remove">Remove</button> </div>');
+        //var a = $("select#filter_choice").val();
+        //$("div#filter_list").append('<div class="filter"><label class="key" id="' + $("select#filter_choice").val() + '">' + $("select#filter_choice").val() + '</label> <input class="value" value=""> <button class="remove">Remove</button> </div>');
+
+        var new_filter = document.createElement("div");
+        new_filter.classList.add("filter");
+
+        var new_label = document.createElement("label");
+        new_label.classList.add("key");
+        new_label.id = $("select#filter_choice").val();
+        new_label.textContent = $("select#filter_choice").val();
+        var input = document.createElement("input");
+        input.classList.add("value");
+        var button = document.createElement("button");
+        button.classList.add("value");
+        button.textContent = "Remove";
+        button.onclick = button_remove;
+
+        new_filter.appendChild(new_label);
+        new_filter.appendChild(input);
+        new_filter.appendChild(button);
+
+        $("div#filter_list").append(new_filter);
+
+        update_filters_list();
     })
 
     var remove = function(foo) {
         $(this).parent().remove();
     }
 
+    update_filters_list();
     update_query();
 
 }(jQuery));
