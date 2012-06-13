d18a4fddd684fd6bb2fc394cef66fce42c6de917
Laurent Peuch <cortex@worlddomination.be>
2012-06-13 02:51:04 +0200

    [enh] add redirection code to avoid breaking old urls

--- apps/votes/urls.py
+++ apps/votes/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, redirect
 
 from votes.models import Proposal, RecommendationData
 
 from votes.feeds import LatestProposalsFeed
+
+
+def dont_broke_old_urls(request, pk):
+    get_object_or_404(Proposal, pk=pk)
+    return redirect(request.path.replace("/votes", "/europe/parliament/vote"))
+
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Proposal), name='index'),
     url(r'^import/$', ListView.as_view(model=RecommendationData), name='import'),
     url(r'^import/(?P<pk>\d+)/$', DetailView.as_view(model=RecommendationData), name='import_vote'),
     url(r'^latest/feed', LatestProposalsFeed(), name='lastest-rss'),
+    url(r'^(?P<pk>[a-zA-Z/-_]+)/(.*)$', dont_broke_old_urls),
 )
