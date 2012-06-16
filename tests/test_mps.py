e241cc1654beec985a90025ab605f5ac8f8f9d91
Laurent Peuch <cortex@worlddomination.be>
2012-06-16 21:43:23 +0200

    [enh] display link to mp votes on a proposal in score listing on detail mp page

--- tests/test_mps.py
+++ tests/test_mps.py
@@ -2,6 +2,7 @@
 from tests import TestCase
 from mps.models import MP
 from django.db.models import Count
+from django.core.urlresolvers import reverse
 
 class TestMPs(TestCase):
 
@@ -28,9 +29,14 @@ class TestMPs(TestCase):
     def test_mps_has_scores_display_title(self):
         mp = MP.objects.all().annotate(score_len=Count('score')).filter(score_len__gt=0)[0]
         resp = self.app.get(mp.get_absolute_url())
-        assert mp.scores[0].proposal.title in resp.pyquery('div#scores div.inner-score')[0].text
+        assert mp.scores[0].proposal.title in resp.pyquery('div#scores div.inner-score a')[0].text
 
     def test_mps_has_score_display_link_to_wiki(self):
         mp = MP.objects.all().annotate(score_len=Count('score')).filter(score_len__gt=0)[0]
         resp = self.app.get(mp.get_absolute_url())
-        assert resp.pyquery("div#scores div.inner-score a")[0].attrib["href"] == "http://www.laquadrature.net/wiki/" + mp.scores[0].proposal.id
+        assert resp.pyquery("div#scores div.inner-score a")[1].attrib["href"] == "http://www.laquadrature.net/wiki/" + mp.scores[0].proposal.id
+
+    def test_mps_has_score_display_link_to_mp_votes_on_proposal(self):
+        mp = MP.objects.all().annotate(score_len=Count('score')).filter(score_len__gt=0)[0]
+        resp = self.app.get(mp.get_absolute_url())
+        assert resp.pyquery("div#scores div.inner-score a")[0].attrib["href"] == reverse("mps:votes_mp", args=[mp.scores[0].proposal.pk, mp.pk])
