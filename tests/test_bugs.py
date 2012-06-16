1d29d0f33a9e399b451c07d8fcbd3007a296dd64
Laurent Peuch <cortex@worlddomination.be>
2012-06-16 03:13:59 +0200

    [test] add a test to check that the rss is working

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -5,3 +5,6 @@ class TestAntiBugs(TestCase):
 
     def test_trends_of_strasser(self):
         self.app.get("/trends/mep/ErnstStrasser.png")
+
+    def test_rss(self):
+        self.app.get("/votes/latest/feed")
