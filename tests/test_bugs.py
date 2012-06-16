7dee8a1be17a3c9e82a06e609b75f9b168edf7ba
Laurent Peuch <cortex@worlddomination.be>
2012-06-16 03:37:03 +0200

    [test] check that recommendation pages of a french proposal is correctly displayed

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -1,4 +1,5 @@
 from tests import TestCase
+from votes.models import Proposal
 
 class TestAntiBugs(TestCase):
 
@@ -7,3 +8,7 @@ class TestAntiBugs(TestCase):
 
     def test_rss(self):
         self.app.get("/votes/latest/feed")
+
+    def test_french_proposals_recommandation(self):
+        id_of_a_recommendation_of_the_proposal = str(Proposal.objects.get(id="Loi_Hadopi_Assemblee_nationale").recommendation_set.all()[0].id)
+        self.app.get("/france/assemblee/vote/Loi_Hadopi_Assemblee_nationale/" + id_of_a_recommendation_of_the_proposal + "/")
