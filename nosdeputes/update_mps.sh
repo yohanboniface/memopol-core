f1cde41180164cb451895e61f595ae1c18d98b3d
Laurent Peuch <cortex@worlddomination.be>
2012-06-16 22:46:44 +0200

    [fix] nosdeputes importation script correct the ||

--- nosdeputes/update_mps.sh
+++ nosdeputes/update_mps.sh
@@ -4,8 +4,13 @@ export PYTHONPATH="../:"$PYTHONPATH
 export PYTHONPATH="../apps/:"$PYTHONPATH
 export DJANGO_SETTINGS_MODULE="memopol2.settings"
 
+fail()
+{
+    echo "import json data into the database failed" && echo "end" && exit 1
+}
+
 echo "Starting the update"
-python ./update_mps.py || ( echo "import json data into the database failed" && echo "end" && exit 1 )
+ipython --pdb ./update_mps.py || fail
 
 echo
 echo "successful end"
