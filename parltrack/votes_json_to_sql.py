10e70e8c76ad16ffec24c6cbf1ed54b218d34117
Laurent Peuch <cortex@worlddomination.be>
2012-07-03 11:54:43 +0200

    [fix] import data form the new ep_votes.json data structure without eating all the system RAM

--- parltrack/votes_json_to_sql.py
+++ parltrack/votes_json_to_sql.py
@@ -3,29 +3,41 @@
 
 import os
 import sys
-from datetime import datetime
-from json import loads
+from dateutil.parser import parse
+from json import loads, dumps
 
 sys.path += [os.path.abspath(os.path.split(__file__)[0])[:-len("parltrack")] + "apps/"]
 
 from votes.models import RecommendationData
+from django.db import transaction
 
 if __name__ == "__main__":
     print "cleaning"
     RecommendationData.objects.all().delete()
     print "read file"
-    lines = open("ep_votes.json").readlines()
+    current_json = ""
     a = 1
-    for vote in lines:
-        jayson = loads(vote)
-        #print jayson.get("report", None), jayson["title"], jayson["ts"]["$date"]
-        RecommendationData.objects.create(proposal_name=jayson.get("report", jayson["title"]),
-                                         title=jayson["title"],
-                                         data=vote,
-                                         date=datetime.fromtimestamp(int(str(jayson["ts"]["$date"])[:-3])))
-        sys.stdout.write("%s/%s\r" % (a, len(lines)))
-        sys.stdout.flush()
-        a += 1
-    sys.stdout.write("\n")
+    with transaction.commit_on_success():
+        for i in open("ep_votes.json", "r"):
+            if i in ("[{\n", "{\n"):
+                #print "begin doc"
+                current_json += "{\n"
+            elif "}\n" == i:
+                #print "end"
+                current_json += "\n}"
+                vote = loads(current_json)
+                RecommendationData.objects.create(proposal_name=vote.get("report", vote["title"]),
+                                                 title=vote["title"],
+                                                 data=dumps(vote, indent=4),
+                                                 date=parse(vote["ts"])),
+                current_json = ""
+                sys.stdout.write("%s\r" % a)
+                sys.stdout.flush()
+                a += 1
+            elif i == ",\n":
+                pass
+            else:
+                current_json += i
+        sys.stdout.write("\n")
 
 # vim:set shiftwidth=4 tabstop=4 expandtab:
