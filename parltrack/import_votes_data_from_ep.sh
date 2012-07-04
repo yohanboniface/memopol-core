473058a5574a1c466aabded87aaf1ca05be8b948
Laurent Peuch <cortex@worlddomination.be>
2012-07-04 16:48:55 +0200

    [fix] correct link to dl votes data

--- parltrack/import_votes_data_from_ep.sh
+++ parltrack/import_votes_data_from_ep.sh
@@ -4,7 +4,9 @@ export PYTHONPATH="../:"$PYTHONPATH
 export DJANGO_SETTINGS_MODULE="memopol2.settings"
 
 echo "[downloading EP votes data file]"
-wget -O ep_votes.json http://parltrack.euwiki.org/dumps/ep_votes.json
+rm ep_votes.json ep_votes.json.xz
+wget -O ep_votes.json.xz http://parltrack.euwiki.org/dumps/ep_votes.json.xz
+unxz ep_votes.json.xz
 echo "[importing data into the database]"
 python ./votes_json_to_sql.py || (echo "import json data into the database failed" && echo "end" && exit 1)
 
