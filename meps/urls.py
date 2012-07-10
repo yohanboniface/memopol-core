ccde1d41b38623b0033325d1ee3b704da0927b5e
Laurent Peuch <cortex@worlddomination.be>
2012-07-10 19:31:51 +0200

    [mod] sort opinions in db instead than in template (doesn't seems to optimize on sqlite)

--- meps/urls.py
+++ meps/urls.py
@@ -31,7 +31,7 @@ urlpatterns = patterns('meps.views',
     url(r'^inactive/$', MEPList.as_view(active=False), name='index_inactive'),
     url(r'^score/$', MEPList.as_view(queryset=MEP.objects.filter(active=True).exclude(total_score__isnull=True).order_by('position'), score_listing=True), name='scores'),
 
-    url(r'^opinion/$', ListView.as_view(queryset=Opinion.objects.filter(institution="EU")), name='index_opinions'),
+    url(r'^opinion/$', ListView.as_view(queryset=Opinion.objects.filter(institution="EU").order_by('-_date')), name='index_opinions'),
     url(r'^opinion/(?P<pk>[0-9]+)/$', DetailView.as_view(model=Opinion, template_name="meps/opinion_detail.html"), name='index_by_opinions'),
     url(r'^organization/$', ListView.as_view(model=Organization), name='index_organizations'),
     url(r'^organization/(?P<pk>[0-9]+)/$', MEPsFromView.as_view(model=Organization, organization_role=True), name='index_by_organization'),
