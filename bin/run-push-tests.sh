30be0d3b7e5403079904146313a82db5ff94134f
Laurent Peuch <cortex@worlddomination.be>
2012-07-11 19:32:01 +0200

    [mod] remove old useless code

--- bin/run-push-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PYTHONPATH="apps/:"$PYTHONPATH
-
-django-admin.py test --settings=memopol2.testsettings --with-coverage --cover-package=memopol2 --with-xunit --with-xcoverage
-django-admin.py  harvest  --settings=memopol2.testsettings  -a memopol2.main
