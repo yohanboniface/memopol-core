b43e34d2902197ff250bf330b504790180146ab6
Laurent Peuch <cortex@worlddomination.be>
2012-07-12 01:11:25 +0200

    [enh] start to optimize mep queries for proposal detail page

--- meps/views.py
+++ meps/views.py
@@ -345,6 +345,7 @@ class ProposalView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProposalView, self).get_context_data(**kwargs)
+        context["vote"].meps = optimise_mep_query(context["vote"].meps)
         return context
 
 
