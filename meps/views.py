463cd5bbc7644d1b01b47afc555fc973a4f6fa9c
Laurent Peuch <cortex@worlddomination.be>
2012-07-12 02:59:50 +0200

    [enh] optimize vote recommendation choice listing with usual mep query optimization, very efficient

--- meps/views.py
+++ meps/views.py
@@ -382,7 +382,7 @@ class VoteRecommendationChoice(VoteRecommendation):
         context = super(VoteRecommendationChoice, self).get_context_data(**kwargs)
         context['choice'] = self.kwargs['recommendation']
         context['header_template'] = 'votes/header_mep_list.html'
-        context['object_list'] = MEP.objects.filter(vote__recommendation=self.object,
-                                  vote__choice=self.kwargs['recommendation'])
+        context['object_list'] = optimise_mep_query(MEP.objects.filter(vote__recommendation=self.object,
+                                                                       vote__choice=self.kwargs['recommendation']))
         self.redirect_args += [self.kwargs['recommendation']]
         return context
