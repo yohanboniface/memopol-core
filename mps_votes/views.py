abb7adb95d518e1fbe941a79d252ed78993b83c3
Laurent Peuch <cortex@worlddomination.be>
2012-07-21 13:10:36 +0200

    [fix] didn't listed correctly mps by choice for a proposal

--- mps_votes/views.py
+++ mps_votes/views.py
@@ -31,8 +31,7 @@ class VoteRecommendationChoice(VoteRecommendation):
     def get_context_data(self, *args, **kwargs):
         context = super(VoteRecommendationChoice, self).get_context_data(**kwargs)
         context['choice'] = self.kwargs['recommendation']
-        context['header_template'] = 'votes/header_mep_list.html'
-        context['object_list'] = MP.objects.filter(vote__recommendation=self.object,
+        context['mps'] = MP.objects.filter(vote__recommendation=self.object,
                                   vote__choice=self.kwargs['recommendation'])
         self.redirect_args += [self.kwargs['recommendation']]
         return context
