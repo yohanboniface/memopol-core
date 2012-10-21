13a4bcdab1dcd126bebe8979dbeb4e99dd69af8e
Yohan Boniface <yb@enix.org>
2012-10-21 17:27:52 +0200

    [enh] Quicly implement the last_votes block on the EU home page

--- memopol2/views.py
+++ memopol2/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
+from votes.models import Proposal
 
 
 class HomeView(TemplateView):
 
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'proposals': Proposal.objects.filter(institution="EU")[:5]
+        }
 
 home = HomeView.as_view()
