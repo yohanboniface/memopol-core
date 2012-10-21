18b1185c81f9c0cafdc764b67bd23fc7477230f1
Yohan Boniface <yb@enix.org>
2012-10-21 21:31:17 +0200

    [enh] First shot in adding the "highlight a MEP" block

--- memopol2/views.py
+++ memopol2/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 from votes.models import Proposal
-from meps.models import Committee
+from meps.models import Committee, MEP
 
 
 class HomeView(TemplateView):
@@ -9,9 +9,12 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
+        # Get a random mep
+        spotlight_mep = MEP.objects.latest('pk')
         return {
             'proposals': Proposal.objects.filter(institution="EU")[:10],
-            'committees': Committee.objects.order_by('abbreviation').all()
+            'committees': Committee.objects.order_by('abbreviation').all(),
+            'spotlight_mep': spotlight_mep
         }
 
 home = HomeView.as_view()
