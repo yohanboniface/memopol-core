dea119d5539e765b634ac7fd8e0912bc1f411a1c
Laurent Peuch <cortex@worlddomination.be>
2012-11-25 16:04:56 +0100

    [enh] data migration for the positions

---
+++ memopol/reps/migrations/0016_most_positions_to_comments.py
@@ -0,0 +1,124 @@
+# encoding: utf-8
+import sys
+import datetime
+from south.v2 import DataMigration
+from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        content_type = ContentType.objects.get(model="mep")
+        site = Site.objects.all()[0]
+        total = orm["reps.opinion"].objects.count()
+        for number, position in enumerate(orm["reps.opinion"].objects.all(), 1):
+            if position.representative_set.count() == 1:
+                Comment.objects.create(
+                    object_pk=position.representative_set.all()[0].pk,
+                    content_type=content_type,
+                    site=site,
+                    comment="<p><b>" + position.title + "</b></p>" + position.content,
+                    ip_address='0.0.0.0',
+                    is_public=True,
+                    is_removed=False,
+                    submit_date=datetime.datetime.now(),
+                    user_url=position.url,
+                )
+            sys.stdout.write("%s/%s\r" % (number, total))
+            sys.stdout.flush()
+
+        sys.stdout.write("\n")
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+
+    models = {
+        'categories.category': {
+            'Meta': {'ordering': "('tree_id', 'lft')", 'unique_together': "(('parent', 'name'),)", 'object_name': 'Category'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'alternate_title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'blank': 'True'}),
+            'alternate_url': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'meta_extra': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'meta_keywords': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['categories.Category']"}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'thumbnail_height': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'thumbnail_width': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'reps.cv': {
+            'Meta': {'object_name': 'CV'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Representative']"}),
+            'title': ('django.db.models.fields.TextField', [], {})
+        },
+        'reps.email': {
+            'Meta': {'object_name': 'Email'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Representative']"})
+        },
+        'reps.opinion': {
+            'Meta': {'object_name': 'Opinion'},
+            '_author': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': "orm['reps.Representative']", 'null': 'True'}),
+            '_date': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institution': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '1023'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '400'})
+        },
+        'reps.opinionrep': {
+            'Meta': {'object_name': 'OpinionREP'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'opinion': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Opinion']"}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Representative']"})
+        },
+        'reps.party': {
+            'Meta': {'object_name': 'Party'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'reps.partyrepresentative': {
+            'Meta': {'object_name': 'PartyRepresentative'},
+            'current': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Party']"}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Representative']"}),
+            'role': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'})
+        },
+        'reps.representative': {
+            'Meta': {'ordering': "['last_name']", 'object_name': 'Representative'},
+            'achievements': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['categories.Category']", 'symmetrical': 'False'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'birth_place': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'local_party': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['reps.Party']", 'through': "orm['reps.PartyRepresentative']", 'symmetrical': 'False'}),
+            'opinions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['reps.Opinion']", 'through': "orm['reps.OpinionREP']", 'symmetrical': 'False'}),
+            'picture': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'reps.website': {
+            'Meta': {'object_name': 'WebSite'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'representative': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reps.Representative']"}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+    }
+
+    complete_apps = ['reps']
