3af1b15bdcb753c8b7a01e0a2e7a0b65e940bc78
Laurent Peuch <cortex@worlddomination.be>
2012-12-16 17:33:04 +0100

    [fix] memleaking in vote importing script

--- memopol/parltrack/management/commands/import_ep_votes_data.py
+++ memopol/parltrack/management/commands/import_ep_votes_data.py
@@ -6,7 +6,7 @@ from os.path import join
 from json import loads, dumps
 from dateutil.parser import parse
 
-from django.db import transaction, connection
+from django.db import transaction, connection, reset_queries
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
@@ -30,28 +30,29 @@ class Command(BaseCommand):
         print "read file"
         current_json = ""
         a = 1
-        with transaction.commit_on_success():
-            # I need to parse the json file by hand, otherwise this eat way to much memory
-            for i in open(join(settings.MEMOPOL_TMP_DIR, "ep_votes.json"), "r"):
-                if i in ("[{\n", "{\n"):
-                    #print "begin doc"
-                    current_json += "{\n"
-                elif "}\n" == i:
-                    #print "end"
-                    current_json += "\n}"
-                    vote = loads(current_json)
-                    RecommendationData.objects.create(proposal_name=vote.get("report", vote["title"]),
-                                                     title=vote["title"],
-                                                     data=dumps(vote, indent=4),
-                                                     date=parse(vote["ts"])),
-                    current_json = ""
-                    sys.stdout.write("%s\r" % a)
-                    sys.stdout.flush()
-                    a += 1
-                elif i == ",\n":
-                    pass
-                else:
-                    current_json += i
-            sys.stdout.write("\n")
+        #with transaction.commit_on_success():
+        # I need to parse the json file by hand, otherwise this eat way to much memory
+        for i in open(join(settings.MEMOPOL_TMP_DIR, "ep_votes.json"), "r"):
+            if i in ("[{\n", "{\n"):
+                #print "begin doc"
+                current_json += "{\n"
+            elif "}\n" == i:
+                #print "end"
+                current_json += "\n}"
+                vote = loads(current_json)
+                RecommendationData.objects.create(proposal_name=vote.get("report", vote["title"]),
+                                                 title=vote["title"],
+                                                 data=dumps(vote, indent=4),
+                                                 date=parse(vote["ts"])),
+                reset_queries()
+                current_json = ""
+                sys.stdout.write("%s\r" % a)
+                sys.stdout.flush()
+                a += 1
+            elif i == ",\n":
+                pass
+            else:
+                current_json += i
+        sys.stdout.write("\n")
 
 # vim:set shiftwidth=4 tabstop=4 expandtab:
