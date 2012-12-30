a3b7b32bcc657f511564ec9cd558246ba90d063e
Laurent Peuch <cortex@worlddomination.be>
2012-12-30 19:28:32 +0100

    [enh] apparently transaction.with_commit_on_success don't nomnom all the memory like I was expecting it to

--- memopol/parltrack/management/commands/import_ep_votes_data.py
+++ memopol/parltrack/management/commands/import_ep_votes_data.py
@@ -30,29 +30,29 @@ class Command(BaseCommand):
         print "read file"
         current_json = ""
         a = 1
-        #with transaction.commit_on_success():
+        with transaction.commit_on_success():
         # I need to parse the json file by hand, otherwise this eat way to much memory
-        for i in open(join(settings.MEMOPOL_TMP_DIR, "ep_votes.json"), "r"):
-            if i in ("[{\n", "{\n"):
-                #print "begin doc"
-                current_json += "{\n"
-            elif "}\n" == i:
-                #print "end"
-                current_json += "\n}"
-                vote = loads(current_json)
-                RecommendationData.objects.create(proposal_name=vote.get("report", vote["title"]),
-                                                 title=vote["title"],
-                                                 data=dumps(vote, indent=4),
-                                                 date=parse(vote["ts"])),
-                reset_queries()
-                current_json = ""
-                sys.stdout.write("%s\r" % a)
-                sys.stdout.flush()
-                a += 1
-            elif i == ",\n":
-                pass
-            else:
-                current_json += i
-        sys.stdout.write("\n")
+            for i in open(join(settings.MEMOPOL_TMP_DIR, "ep_votes.json"), "r"):
+                if i in ("[{\n", "{\n"):
+                    #print "begin doc"
+                    current_json += "{\n"
+                elif "}\n" == i:
+                    #print "end"
+                    current_json += "\n}"
+                    vote = loads(current_json)
+                    RecommendationData.objects.create(proposal_name=vote.get("report", vote["title"]),
+                                                     title=vote["title"],
+                                                     data=dumps(vote, indent=4),
+                                                     date=parse(vote["ts"])),
+                    reset_queries()
+                    current_json = ""
+                    sys.stdout.write("%s\r" % a)
+                    sys.stdout.flush()
+                    a += 1
+                elif i == ",\n":
+                    pass
+                else:
+                    current_json += i
+            sys.stdout.write("\n")
 
 # vim:set shiftwidth=4 tabstop=4 expandtab:
