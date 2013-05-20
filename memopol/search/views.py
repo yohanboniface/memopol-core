e0ff53e6a22c44410b6a5ad270f1b8a9ec6558fa
turbobilly <turbobilly@hush.com>
2013-05-20 14:47:31 +0200

    Cleanups

--- memopol/search/views.py
+++ memopol/search/views.py
@@ -79,11 +79,7 @@ class SearchView(TemplateView):
                 "label": label,
                 "formset": formset,
                 "form": form,
-<<<<<<< HEAD
                 "average": average,
-=======
-				"average": average,
->>>>>>> Bug #320 - Add the average score to the search result page
                 "shortcuts": [
                     TopRated({"request": self.request}),
                     WorstRated({"request": self.request})
